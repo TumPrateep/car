6d07f6548fec7ede4963e2b12d5d818474751bc0
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-05-16 16:59:28 +0700

    update insert delete api

--- application/controllers/api/spares.php
+++ application/controllers/api/spares.php
@@ -58,5 +58,141 @@ class spares extends BD_Controller {
     }
 
 
+    function createSpares_post(){
+
+        $sparesbrandName = $this->post("sparesName");
+        
+        $this->load->model("spares");
+        $isCheck = $this->spares->getBrandforTF($sparesName);
+
+        if($isCheck){
+            $data = array(
+                'sparesId' => null,
+                'sparesName' => $sparesName
+            );
+            $result = $this->Spare->insertSpares($data);
+            $output["status"] = $result;
+            if($result){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+            else{
+                $output["status"] = false;
+                $output["message"] = REST_Controller::MSG_NOT_CREATE;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+
+        }
+        else{
+            $output["status"] = false;
+            $output["message"] = REST_Controller::MSG_CREATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+
+    function updateSpare_post(){
+
+        $sparesId = $this->post('sparesId');
+        $spareName = $this->post('sparesName');
+        
+        $this->load->model("spares");
+
+        $result = $this->spares->wherenotspares($sparesId,$sparesName);
+
+        if($result){
+            $data = array(
+                'sparesId' => $sparesId,
+                'sparesName' => $sparesName
+            );
+            $result = $this->Spare->updateSpares($data);
+            $output["status"] = $result;
+            if($result){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+            else{
+                $output["status"] = false;
+                $output["message"] = REST_Controller::MSG_NOT_UPDATE;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_UPDATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+
+    function deleteSpareBrand_get(){
+        $sparesbrandId = $this->get('sparesId');
+
+        $this->load->model("spares");
+        $sparBrand = $this->spares->getSparebyId($sparesId);
+        if($sparBrand != null){
+            $isDelete = $this->Spare->delete($sparesId);
+            if($isDelete){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["message"] = REST_Controller::MSG_BE_USED;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_BE_DELETED;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+    function getSpares_post(){
+
+        $sparesName = $this->post('sparesName');
+        $this->load->model("spares");
+        $isCheck = $this->spares->checkSpare($sparesName);
+
+        if($isCheck){
+            $output["status"] = true;
+            $result = $this->spares->getSpares($sparesName);
+            if($result != null){
+                $output["data"] = $result;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["message"] = REST_Controller::MSG_ERROR;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["status"] = false;
+            $output["message"] = REST_Controller::MSG_ERROR;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+    function getSpare_post(){
+
+        $sparesId = $this->post('sparesId');
+        $this->load->model("spares");
+        $isCheck = $this->spares->getSparebyId($sparesId);
+
+        if($isCheck){
+            $output["status"] = true;
+            $result = $this->Spare->getSpare($sparesId);
+            if($result != null){
+                $output["data"] = $result;
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["status"] = false;
+                $output["message"] = REST_Controller::MSG_BE_DELETED;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["status"] = false;
+            $output["message"] = REST_Controller::MSG_BE_DELETED;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+
+
+    }
+
+
 
     }
--- application/models/spares.php
+++ application/models/spares.php
@@ -63,6 +63,103 @@ class Spares extends CI_Model{
         return $query->num_rows();
     }
 
+
+    function insertSpares($data){
+        $result = $this->db->insert('spares', $data);
+        return $result;
+    }
+
+    function getBrandforTF($sparesName){
+        $this->db->select("sparesName");
+        $this->db->from("spares");
+        $this->db->where('sparesName', $sparesName);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return false;
+        }
+        return true;
+    }
+
+    function wherenotspares($sparesId,$sparesName){
+        $this->db->select("sparesName");
+        $this->db->from("spares");
+        $this->db->where('sparesName', $sparesName);
+        $this->db->where_not_in('sparesdId', $sparesdId);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return false;
+        }
+        return true;
+    }
+
+    function updateSpares($data){
+        $this->db->where('sparesId',$data['sparesdId']);
+        $result = $this->db->update('spares', $data);
+        return $result;
+    }
+
+    function getSparebyId($sparesId){
+        $this->db->where('sparesId',$sparesId);
+        $result = $this->db->get('spares')->row();
+        return $result;
+    }
+
+    function delete($sparesId){
+        return $this->db->delete('spares', array('sparesId' => $sparesId));
+    }
+
+    function checkSpares($sparesName) {
+        $this->db->select("*");
+        $this->db->from("spares");
+        $this->db->where('sparesName',$sparesName);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    
+    function getSpares($sparesName){
+        return $this->db->where('sparesName',$sparesName)->get("spares")->row();
+    }
+
+
+    function checkSpare($sparesName) {
+        $this->db->select("*");
+        $this->db->from("spares");
+        $this->db->where('sparesName',$sparesName);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    function getSparebyId($sparesId){
+        $this->db->select("sparesId");
+        $this->db->from("spares");
+        $this->db->where('sparesId', $$sparesId);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return false;
+        }
+        return true;
+    }
+
+    function getSpare($sparesId){
+        $this->db->where('sparesId',$sparesId);
+        $result = $this->db->get('spares')->row();
+        return $result;
+    }
    
 
 }
