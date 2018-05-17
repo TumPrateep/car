90cc1fed85d6a40971097ee7239572330bbd219b
tumtpk <pk.prateep@gmail.com>
2018-05-17 11:39:48 +0700

    add check role

---
+++ application/controllers/Role.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Role extends CI_Controller {
+
+	function __construct()
+    {
+        // Construct the parent class
+		parent::__construct();
+		$this->load->view("lib");
+		if(!isset($this->session->userdata['logged_in'])){
+			redirect("auth/login");
+		}
+    }
+
+    function index(){
+        $role = $this->session->userdata['logged_in']['role'];
+        if($role == 1){
+            // Admin
+            redirect("admin");
+        }else if($role == 2){
+            // ร้านอะไหล่
+            redirect("caraccessory");
+        }else if($role == 3){
+            // อู่
+            redirect("garage");
+        }else if($role == 4){
+            // ผู้ใช้งาน
+            redirect("user");
+        }else{
+            // ไม่มี role
+            redirect("userprofile");
+        }
+    }
+
+}
