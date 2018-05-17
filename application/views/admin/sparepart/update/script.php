d747603b5013dfa5ae0163feb9ab25b73a9bc7f8
sunsunnp <sunsun.np@gmail.com>
2018-05-17 17:04:56 +0700

    frontend updateSpare

--- application/views/admin/sparepart/update/script.php
+++ application/views/admin/sparepart/update/script.php
@@ -1,4 +1,26 @@
 <script>
+
+
+    var spares_brandId = $("#spares_brandId").val();
+    var spares_undercarriageId = $("#spares_undercarriageId").val();
+
+    $.post(base_url+"api/SparePartCar/getBrand",{
+        "spares_brandId": spares_brandId,
+        "spares_undercarriageId" : spares_undercarriageId
+    },function(data){
+        if(data.message!=200){
+            showMessage(data.message,"SparePartCar/sparepart/"+spares_undercarriageId);
+        }
+
+        if(data.message == 200){
+            result = data.data;
+            $("#spares_brandName").val(result.spares_brandName);
+        }
+        
+    });
+
+
+
     $("#spares").validate({
         rules: {
             sparesbrandName: {
@@ -11,28 +33,30 @@
             }
         },
     });
+  
     
     $("#spares").submit(function(){
-        createSpares();
+        updateBrand();
     })
 
-    function createSpares(){
+
+    function updateBrand(){
         event.preventDefault();
         var isValid = $("#spares").valid();
         
         if(isValid){
             var data = $("#spares").serialize();
-            $.post(base_url+"api/sparepartcar/createSpareBrand",data,
+            $.post(base_url+"api/SparePartCar/updateSpareBrand",data,
             function(data){
-                if(data.message == 200){
-                    showMessage(data.message,"sparepartcar");
-                }else{
-                    showMessage(data.message);
-                }
+                // var spares_brandId = $("#spares_brandId").val();
+                showMessage(data.message,"SparePartCar/sparepart/"+spares_undercarriageId);
             });
             
         }
     }
+
+
+
     
 
 </script>
