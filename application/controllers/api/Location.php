853667bdfe6af2ea1c6954cb6146378bdcf7094f
sunsunnp <sunsun.np@gmail.com>
2018-05-18 14:30:02 +0700

    api backend location subdistrict

--- application/controllers/api/Location.php
+++ application/controllers/api/Location.php
@@ -8,9 +8,26 @@ class Location extends BD_Controller {
     {
         // Construct the parent class
         parent::__construct();
-        $this->auth();
+        // $this->auth();
     }
 
+    function getSubdistrict_post(){
+
+        $districtId = $this->post("districtId");
+
+        $this->load->model("LocationModel");
+        $output["status"] = true;
+        $result = $this->LocationModel->getSubdistrict($districtId);
+        if($result != null){
+            $output["data"] = $result;
+            $output["message"] = REST_Controller::MSG_SUCCESS;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }else{
+            $output["status"] = false;
+            $output["message"] = REST_Controller::MSG_BE_DELETED;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
 
 
 
--- application/models/locationModel.php
+++ application/models/locationModel.php
@@ -7,4 +7,14 @@ class LocationModel extends CI_Model {
         
     }
 
+    function getSubdistrict($districtId){
+        $this->db->from('subdistrict');
+        $this->db->where('districtId',$districtId);
+        $this->db->where('status',1);
+        $this->db->order_by("subdistrictName", "asc");
+        $result = $this->db->get();
+        return $result->result();
+
+    }
+
 }
