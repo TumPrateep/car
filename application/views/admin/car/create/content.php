15f29b89ee2a856b57f13583a6fa3f1c0667f144
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-05-21 16:50:19 +0700

    aa

--- application/views/admin/car/create/content.php
+++ application/views/admin/car/create/content.php
@@ -10,39 +10,41 @@
     </ol>
     <!-- Icon Cards-->
     <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-10">
-            <div class="card text-white bg-success">
-                <div class="card-header">
-
-                  <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เพิ่มข้อมูลยี่ห้อรถ</h3>
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form id="create-brand" enctype="multipart/form-data" >
-                <div class="card-body black bg-light">
-                    <div class="form-group">
-                      <label>ชื่อยี่ห้อรถ</label>
-                      <input type="text" class="form-control" placeholder="ชื่อยี่ห้อรถ" name="brandName">
-                    </div>
-                    <div class="container kv-main ">
-                        <form enctype="multipart/form-data">
-                            <input id="input-b3" name="input-b3[]" type="file" class="file" multiple 
-                        data-show-upload="false" data-show-caption="true" data-msg-placeholder="Select {files} for upload...">
-                                <br>
-                            </form>
-                    </div>
-                    <div class="form-group">
-                      <button type="submit" class="btn btn-primary">บันทึก</button>
-                    </div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-10">
+              <div class="card text-white bg-success">
+                  <div class="card-header">
+                    <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เพิ่มข้อมูลยี่ห้อรถ</h3>
                   </div>
-                </form> 
-                  <!-- /.card-body -->
-               
-                </div>
-            </div>
+                  <!-- /.card-header -->
+                  <!-- form start -->
+                  <form id="create-brand" enctype="multipart/form-data" >
+                  <div class="card-body black bg-light">
+                      <div class="form-group">
+                        <label>ชื่อยี่ห้อรถ</label>
+                        <input type="text" class="form-control" placeholder="ชื่อยี่ห้อรถ" name="brandName">
+                      </div>
+                      <div class="container kv-main ">
+                      <form enctype="multipart/form-data">
+                      
+                      <input id="input-b3" name="input-b3[]" type="file" class="file" multiple 
+                      data-show-upload="false" data-show-caption="true" data-msg-placeholder="Select {files} for upload...">
+                      
+                      </form>
+                       <br>
+                      </div>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary">บันทึก</button>
+                      </div>
+                    </div>
+                    
+                  </form> 
+                    <!-- /.card-body -->
+                 
+                  </div>
+              </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
   </div>
