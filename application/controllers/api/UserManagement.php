689bc224d1dfe10a374bb3d58332be3cb7b3a543
Nattaphon_EnterHelloWorld <natlove.natlove202@gmail.com>
2018-05-24 13:42:34 +0700

    ดรป พนฟส

--- application/controllers/api/UserManagement.php
+++ application/controllers/api/UserManagement.php
@@ -52,11 +52,11 @@ class UserManagement extends BD_Controller {
                 $nestedData['phone'] = $post->phone;
                 $nestedData['email'] = $post->email;
                 switch($post->category){
-                    case "1" : $nestedData['category'] ="admin";break;
-                    case "2" : $nestedData['category'] ="caraccessory";break;
-                    case "3" : $nestedData['category'] ="garage";break;
-                    case "4" : $nestedData['category'] ="user";break;
-                    default  : $nestedData['category'] ="no category";break;
+                    case "1" : $nestedData['category'] ="ผู้ดูแลระบบ";break;
+                    case "2" : $nestedData['category'] ="ร้านอะไหล่";break;
+                    case "3" : $nestedData['category'] ="อู่";break;
+                    case "4" : $nestedData['category'] ="ผู้ใช้";break;
+                    default  : $nestedData['category'] =" ";break;
 
                     
                 }
@@ -220,144 +220,7 @@ class UserManagement extends BD_Controller {
     }
 
     function userTypeAndData_post(){
-        $role = (int)$this->post("role");
-
-        $config['allowed_types'] = 'gif|jpg|png';
-        // $config['max_size'] = '100';
-        $config['overwrite'] = TRUE;
-        $config['encrypt_name'] = TRUE;
-        $config['remove_spaces'] = TRUE;
-
-        $firstname = $this->post("firstname");
-        $lastname = $this->post("lastname");
-        $address = $this->post("address");
-        $provinceId = $this->post("provinceId");
-        $districtId = $this->post("districtId");
-        $subdistrictId = $this->post("subdistrictId");
-        $phone1 = $this->post("phone1");
-        $phone2 = $this->post("phone2");
-
-        $userId = $this->session->userdata['logged_in']['id'];
-
-        $this->load->model("Profile");
-    
-        $profileData = array(
-            'user_profile' => null,
-            'firstname' => $firstname,
-            'lastname' => $lastname,
-            'status' => 1,
-            'address' => $address,
-            'phone1' => $phone1, 
-            'phone2' => $phone2,
-            'provinceId' => $provinceId, 
-            'districtId' => $districtId,
-            'subdistrictId' => $subdistrictId,
-            'create_by' => $userId,
-            'create_at' => date('Y-m-d H:i:s',time()),
-            'userId' => $userId
-        );
-
-        if($role == 3){
-
-        }else if($role == 2){
-            $car_accessoriesName = $this->post("car_accessoriesName");
-            $businessRegistrationAccessories = $this->post("businessRegistrationAccessories");
-            $roleData = array(
-                'car_accessoriesId' => null,
-                'car_accessoriesName' => $car_accessoriesName,
-                'businessRegistration' => $businessRegistrationAccessories,
-                'userId' => $userId,
-                'create_at' => date('Y-m-d H:i:s',time()),
-                'create_by' => $userId,
-                'update_at' => null,
-                'update_by' => null,
-                'status' => 1  
-            );
-        }else if($role == 4){
-            $path = 'public/image/car_profile/'.$userId;
-            mkdir($path);
-            $config['upload_path'] = $path;
-
-            $frontPicture = $this->post("frontPicture");
-            $backPicture = $this->post("backPicture");
-            $circlesignPicture = $this->post("circlesignPicture");
-            $characterPlate = $this->post("characterPlate");
-            $numberPlate = $this->post("numberPlate");
-            $provincePlate = $this->post("provincePlate");
-            $mileage = $this->post("mileage");
-            $colorCar = $this->post("colorCar");
-
-            $this->load->library('upload', $config);
-            $pictureFrontName = null;
-            $pictureBackName = null;
-            $circlePlateName = null;
-            if($frontPicture != "undefined"){
-                if ( ! $this->upload->do_upload("frontPicture")){
-                    $error = array('error' => $this->upload->display_errors());
-                    $output["message"] = REST_Controller::MSG_ERROR;
-                    $output["data"] = $error;
-                    $this->set_response($output, REST_Controller::HTTP_OK);
-                }else{
-                    $imageDetailArray = $this->upload->data();
-                    $pictureFrontName =  $imageDetailArray['file_name'];
-                }      
-            }
-
-            if($backPicture != "undefined"){
-                if ( ! $this->upload->do_upload("backPicture")){
-                    $error = array('error' => $this->upload->display_errors());
-                    $output["message"] = REST_Controller::MSG_ERROR;
-                    $output["data"] = $error;
-                    $this->set_response($output, REST_Controller::HTTP_OK);
-                }else{
-                    $imageDetailArray = $this->upload->data();
-                    $pictureBackName =  $imageDetailArray['file_name'];
-                }
-            }
-
-            if($circlesignPicture != "undefined"){
-                if ( ! $this->upload->do_upload("circlesignPicture")){
-                    $error = array('error' => $this->upload->display_errors());
-                    $output["message"] = REST_Controller::MSG_ERROR;
-                    $output["data"] = $error;
-                    $this->set_response($output, REST_Controller::HTTP_OK);
-                }else{
-                    $imageDetailArray = $this->upload->data();
-                    $circlePlateName =  $imageDetailArray['file_name'];
-                }
-            }
-
-            $roleData = array(
-                "car_profileId" => null,
-                "mileage" => $mileage,
-                "pictureFront" => $pictureFrontName,
-                "pictureBack" => $pictureBackName,
-                "circlePlate" => $circlePlateName,
-                "create_at" => date('Y-m-d H:i:s',time()),
-                "create_by" => $userId,
-                "userId" => $userId,
-                "update_at" => null,
-                "update_by" => null,
-                "status" => 1,
-                "character_plate" => $characterPlate,
-                "number_plate" => $numberPlate,
-                "province_plate" => $provincePlate,
-                "color" => $colorCar
-            );
-        }else{
-            $roleData = null;
-        }
-
-        $result = $this->Profile->saveProfileRoleUser($role, $userId, $profileData, $roleData);
-        if($result){
-            $output["message"] = REST_Controller::MSG_SUCCESS;
-        }else{
-            $output["message"] = REST_Controller::MSG_ERROR;
-        }
-        $this->set_response($output, REST_Controller::HTTP_OK);
-        
-        
-
+        $role = $this->post("role");
 
         // if($role == "3"){
         //     $config['upload_path'] = 'public/image/brand/';
@@ -430,8 +293,32 @@ class UserManagement extends BD_Controller {
         //             $this->set_response($output, REST_Controller::HTTP_OK);
         //         }
         //     }
-        // }
-        //else{
+        // }else if($role == "2"){
+
+        //     $car_accessoriesName = $this->post("car_accessoriesName");
+        //     $businessRegistration = $this->post("businessRegistration");
+        //     $create_add = $this->session->userdata['logged_in']['id'];
+
+        //     $this->db->trans_start();
+        //     $data = array(
+        //         'car_accessoriesId' => null,
+        //         'car_accessoriesName' => $car_accessoriesName,
+        //         'businessRegistration' => $businessRegistration,
+        //         'id' => $create_add,
+        //         'create_add' => date('Y-m-d H:i:s',time()),
+        //         'create_by' => $create_add,
+        //         'status' => 1
+        //     );
+        //     $isResult = $this->User->insert_role2($data);
+        //     $this->db->trans_complete();
+        //     if($isResult){
+        //         $output["message"] = REST_Controller::MSG_SUCCESS;
+        //         $this->set_response($output, REST_Controller::HTTP_OK);
+        //     }else{
+        //         $output["message"] = REST_Controller::MSG_NOT_CREATE;
+        //         $this->set_response($output, REST_Controller::HTTP_OK);
+        //     }
+        // }else{
         //     $config['upload_path'] = 'public/image/brand/';
         //     $config['allowed_types'] = 'gif|jpg|png';
         //     // $config['max_size'] = '100';
