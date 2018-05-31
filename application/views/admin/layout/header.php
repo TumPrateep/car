284400407d1830bdde6fb7dd845d29da61e7c8d8
tumtpk <pk.prateep@gmail.com>
2018-05-31 14:42:15 +0700

    edit create api

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -1,5 +1,14 @@
 
       <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-fw fa-user-circle-o"></i> dasdasdasdasd
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
+              <a class="dropdown-item small" href="#">แก้ไขข้อมูล</a>
+              <a class="dropdown-item small" href="#">View all messages</a>
+          </div>
+        </li>
         <li class="nav-item">
           <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
             <i class="fa fa-fw fa-sign-out"></i>Logout</a>
