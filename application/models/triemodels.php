fece1bffda0fd9bda39a6069de52d3a0f987b051
sunsunnp <sunsun.np@gmail.com>
2018-06-02 15:26:09 +0700

    add create tiremodel api

--- application/models/triemodels.php
+++ application/models/triemodels.php
@@ -16,4 +16,23 @@ class Triemodels extends CI_Model {
         return $this->db->delete('tire_model', array('tire_modelId' => $tire_modelId));
     }
 
+    function get_Tiremodel($trie_brandId,$tire_modelName){
+        $this->db->select("tire_modelName");
+        $this->db->from("tire_model");
+        $this->db->where('tire_modelName', $tire_modelName);
+        $this->db->where('trie_brandId', $trie_brandId);
+        $result = $this->db->count_all_results();
+
+        if($result > 0){
+            return false;
+        }
+        return true;
+    }
+
+    function insert_Tiremodel($data){
+        $result = $this->db->insert('tire_model', $data);
+        return $result;
+    }
+
+
 }
