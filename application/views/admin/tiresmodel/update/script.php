6f46066766852e8caa2f5c033842b53ec53aa6c2
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2018-06-02 17:55:14 +0700

    validate

--- application/views/admin/tiresmodel/update/script.php
+++ application/views/admin/tiresmodel/update/script.php
@@ -1,4 +1,17 @@
 <script>
+     $("#spares").validate({
+            rules: {
+                tire_brandName: {
+                    required: true
+                },
+            },
+            messages: {
+                tire_brandName: {
+                    required: "กรุณากรอกยี่ห้อยาง"
+                }
+            },
+        });
+  
 
     
 </script>
