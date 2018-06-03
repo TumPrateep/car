1db72feefc6a0ad758396fd75864da776340161d
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2018-06-03 17:01:10 +0700

    add

--- application/views/admin/tiresbrand/update/content.php
+++ application/views/admin/tiresbrand/update/content.php
@@ -1,13 +1,9 @@
 
-
     <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="<?=base_url("admin/car") ?>">การจัดการยี่ห้อ</a>
-        </li>
-        <li class="breadcrumb-item">
-          <a href="<?=base_url("admin/car/model/$brandId") ?>">รุ่นรถ</a>
+          <a href="<?=base_url("admin/tires/tiresbrand/") ?>">การจัดการยี่ห้อยางรถยนตร์</a>
         </li>
         <li class="breadcrumb-item active">เเก้ไขข้อมูล</li>
       </ol>
@@ -18,40 +14,26 @@
             <div class="col-md-10">
               <div class="card text-white bg-success">
                   <div class="card-header">
-
-                    <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เเก้ไขข้อมูลรุ่น</h3>
+                    <h3 class="card-title"><i class="fa fa-futbol-o"></i>  เเก้ไขข้อมูลยี่ห้อยางรถยนตร์</h3>
                   </div>
                   <!-- /.card-header -->
                   <!-- form start -->
-                  <form id="submit">
-                    <input type="hidden" id="brandId" name="brandId" value="<?=$brandId ?>">
-                    <input type="hidden" id="modelId" name="modelId" value="<?=$modelId ?>">
-                    <div class="card-body black bg-light">
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <label>ชื่อรุ่น</label> <span class="error">*</span>
-                            <input type="text" class="form-control" placeholder="ชื่อรุ่นรถ" id="modelName" name="modelName" value="">
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <label>ปีที่ผลิต</label> <span class="error">*</span> <label id="yearStart-error" class="error" for="yearStart"></label>
-                            <div class="form-inline">
-                              <select class="form-control col-md-5" name="yearStart" id="yearStart"></select>
-                              <label class="col-md-2">ถึง</label>
-                              <select class="form-control col-md-5" name="yearEnd" id="yearEnd"></select>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+
+                  <form id="update-tiresbrand" enctype="multipart/form-data" >
+                  <div class="card-body black bg-light">
                       <div class="form-group">
-                        <button type="submit" class="btn btn-primary ">บันทึก</button>
+                        <label>ชื่อยี่ห้อยางรถยนตร์</label> <span class="error">*</span>
+                        <input type="text" class="form-control" placeholder="ชื่อยี่ห้อยางรถยนตร์" name="tiresbrandName">
+                      </div>
+                        <div class="form-group"> 
+                          <input id="brandPicture" name ="tiresbrandPicture" class="file" type="file">
+                        </div>
+                      <div class="form-group"> 
+                        <button type="submit" class="btn btn-primary">บันทึก</button>
                       </div>
                     </div>
-                    </form>
+                    
+                  </form>
                     <!-- /.card-body -->
                  
                 </div>
