07179a8cefa696476abddbd774218637acaf6496
Yodev <natlove.natlove202@gmail.com>
2018-06-04 12:02:19 +0700

    update

--- application/views/admin/sizetires/content.php
+++ application/views/admin/sizetires/content.php
@@ -25,7 +25,7 @@
         </div>
     </div>
 
-    <input type="hidden" id="tire_sizeId" value="<?=$tire_sizeId ?>">
+    <input type="hidden" id="rimId" name="rimId" value="<?=$rimId ?>">
   
     <div class="table-responsive">
       <table class="table table-bordered" id="tiresize-table" width="100%" cellspacing="0">
