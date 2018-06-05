34780506c71dedde6dcf493286ccca0f3f46692e
mahdez <mahdee.dark@gmail.com>
2018-06-05 11:11:03 +0700

    แก้ไขเพิ่มข้อมูลขอบยาง แก้ไขคำหน้าขอบยาง

--- application/views/admin/sizetires/update/content.php
+++ application/views/admin/sizetires/update/content.php
@@ -3,13 +3,11 @@
     <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
+       <li class="breadcrumb-item">
+          <a href="<?=base_url("admin/tires") ?>">ขอบยาง</a>
         <li class="breadcrumb-item">
-          <a href="<?=base_url("admin/car") ?>">การจัดการยี่ห้อ</a>
-        </li>
-        <li class="breadcrumb-item">
-          <a href="<?=base_url("admin/car/model/$brandId") ?>">รุ่นรถ</a>
-        </li>
-        <li class="breadcrumb-item active">เเก้ไขข้อมูล</li>
+          <a href="<?=base_url("admin/Tires/tiresize/$rimId") ?>">ขนาดยาง</a>
+        <li class="breadcrumb-item active">เเก้ไขขอมูลขอบยาง</li>
       </ol>
       <!-- Icon Cards-->
       <section class="content">
@@ -19,7 +17,7 @@
               <div class="card text-white bg-success">
                   <div class="card-header">
 
-                    <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เเก้ไขข้อมูลรุ่น</h3>
+                    <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เเก้ไขข้อมูลขอบยาง</h3>
                   </div>
                   <!-- /.card-header -->
                   <!-- form start -->
@@ -30,20 +28,8 @@
                       <div class="row">
                         <div class="col-md-12">
                           <div class="form-group">
-                            <label>ชื่อรุ่น</label> <span class="error">*</span>
-                            <input type="text" class="form-control" placeholder="ชื่อรุ่นรถ" id="modelName" name="modelName" value="">
-                          </div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <label>ปีที่ผลิต</label> <span class="error">*</span> <label id="yearStart-error" class="error" for="yearStart"></label>
-                            <div class="form-inline">
-                              <select class="form-control col-md-5" name="yearStart" id="yearStart"></select>
-                              <label class="col-md-2">ถึง</label>
-                              <select class="form-control col-md-5" name="yearEnd" id="yearEnd"></select>
-                            </div>
+                            <label>ระบุขนาดยาง</label> <span class="error">*</span>
+                            <input type="text" class="form-control" placeholder="ระบุขนาดยาง" id="modelName" name="modelName" value="">
                           </div>
                         </div>
                       </div>
