7dbdb9692c7b5cc06d7b32995f699416b14ebeb9
puth1998 <Teenaputh12@hotmail.com>
2018-06-06 14:38:02 +0700

    view user

--- application/views/admin/user/view/content.php
+++ application/views/admin/user/view/content.php
@@ -18,31 +18,12 @@
               <div class="col-md-10">
                 <div class="card text-white bg-success">
                     <div class="card-header">
-                      <h3 class="card-title"><i class="fa fa-user-circle-o" ></i>รายข้อมูลผู้ใช้งาน</h3>
+                      <h3 class="card-title"><i class="fa fa-user-circle-o" ></i> รายข้อมูลผู้ใช้งาน</h3>
                     </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
-                    <!-- <form id="group"> -->
+                   
                       <div class="card-body black bg-light">
-                        <div class="form-group">
-                          <label>ชื่อผู้ใช้งาน</label>
-                          <input type="text" class="form-control" placeholder="ชื่อผู้ใช้งาน" name="username">
-                        </div>
-                        <div class="form-group">
-                          <label>เบอร์โทรศัพท์</label> <span class="error">*</span>
-                          <input type="text" class="form-control" placeholder="เบอร์โทรศัพท์" name="phoneNumber"> 
-                        </div>
-                        <div class="form-group">
-                          <label>อีเมล</label>
-                          <input type="text" class="form-control" name="email" placeholder="อีเมล">
-                        </div>
                         
-                        <div class="form-group">
-                          <button type="submit" class="btn btn-primary">บันทึก</button>
-                        </div>
                       </div>
-                      <!-- </form> -->
-                      <!-- /.card-body -->
                    
                   </div>
                 </div>
