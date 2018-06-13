d6dea831c080e1bb0f8f21369484ba178db250f9
Yodev <natlove.natlove202@gmail.com>
2018-06-13 14:47:45 +0700

    view spare undercarriegs

--- application/controllers/caraccessory/Spareundercarries.php
+++ application/controllers/caraccessory/Spareundercarries.php
@@ -1,24 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Car extends CI_Controller {
+class Spareundercarries extends CI_Controller {
 
 	function __construct()
     {
         // Construct the parent class
 		parent::__construct();
 		$this->load->view("lib");
-    }
+	}
 	
-	public function index()
+	public function spareUndercarriage()
 	{
 		$this->load->view("caraccessory/layout/head");
 		$this->load->view("caraccessory/layout/header");
 		$this->load->view("caraccessory/layout/left-menu");
-		$this->load->view("caraccessory/car/content");
+		$this->load->view("caraccessory/spareUndercarriage/content");
 		$this->load->view("caraccessory/layout/footer");
 		$this->load->view("caraccessory/layout/foot");
 	}
-
 	
+
 }
+
+
+
 
