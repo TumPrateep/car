e9ae460b080c700e7ab43a2c0a9de5776ceca1c2
tumtpk <pk.prateep@gmail.com>
2018-06-14 09:53:35 +0700

    add menu

--- application/views/caraccessory/layout/left-menu.php
+++ application/views/caraccessory/layout/left-menu.php
@@ -6,8 +6,9 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
+                        <li class="nav-label">ข้อมูลทั่วไป</li>
                         <li><a href="<?=base_url("caraccessory/car"); ?>"><i class="fa fa-tachometer"></i><span class="hide-menu">การจัดการยี่ห้อ/รุ่นรถ</span></a></li>
-                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">ข้อมูลอะไหล่ <span class="label label-rouded label-primary pull-right">2</span></span></a>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">ข้อมูลอะไหล่ </span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="<?=base_url("caraccessory/car/spareUndercarriage"); ?>">อะไหล่ช่วงล่าง </a></li>
                                 <li><a href="<?=base_url("caraccessory/lubricator"); ?>">น้ำมันเครื่อง </a></li>
@@ -15,10 +16,17 @@
                                     <ul aria-expanded="true" class="collapse in" style="">
                                         <li><a href="#">ขอบ/ขนาดยาง</a></li>
                                         <li><a href="#">ยี่ห้อ/รุ่นยาง</a></li>
-                                        <li><a href="#">ข้อมูลยาง</a></li>
                                     </ul>
                                 </li>
                             </ul>
+                        </li>
+                        <li class="nav-label">ส่วนของร้านอะไหล่</li>
+                        <li><a href="#"><i class="fa fa-tachometer"></i><span class="hide-menu">ข้อมูลอะไหล่ช่วงล่าง</span></a></li>
+                        <li><a href="#"><i class="fa fa-tachometer"></i><span class="hide-menu">ข้อมูลน้ำมันเครื่อง</span></a></li>
+                        <li>
+                            <a href="<?=base_url("caraccessory/tiredata"); ?>">
+                                <i class="fa fa-tachometer"></i><span class="hide-menu">ข้อมูลยาง</span>
+                            </a>
                         </li>
                     </ul>
                 </nav>
---
+++ application/views/caraccessory/tiredata/content.php
@@ -0,0 +1,63 @@
+<!-- Page wrapper  -->
+<div class="page-wrapper">
+    <!-- Bread crumb -->
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h3 class="text-primary">ยี่ห้อรถ</h3>  
+        </div>
+        <div class="col-md-7 align-self-center">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/tiredata"); ?>">ข้อมูลยาง</a></li>
+                <li class="breadcrumb-item active">ค้นหา</li>
+            </ol>
+        </div>
+    </div>
+    <!-- End Bread crumb -->
+    <!-- Container fluid  -->
+    <div class="container-fluid">
+        <div class="row bg-white p-30 search-header">
+            <div class="col-md-3">
+                <a href="<?=base_url("caraccessory/car/createcar") ?>">
+                    <button type="button" class="btn btn-info m-b-10 m-l-5" ><i class="fa fa-plus">  เพิ่ม</i></button>
+                </a>     
+            </div>
+            <div class="col-md-2">
+                <div class="input-group input-group-default">
+                    <select class="form-control">
+                        <option>1</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="input-group input-group-default">
+                    <select class="form-control">
+                        <option>1</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="input-group input-group-default">
+                    <select class="form-control">
+                        <option>1</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="input-group input-group-default">
+                    <select class="form-control">
+                        <option>1</option>
+                    </select>
+                    <span class="input-group-btn"><button class="btn btn-primary" id="btn-search"><i class="ti-search"></i></button></span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="table">
+            <table class="table table-bordered" id="brand-table" width="100%" cellspacing="0">
+                <thead>
+                    <th></th>
+                </thead>	
+            </table>
+        </div>
+    </div>
+    <!-- End Container fluid  -->
