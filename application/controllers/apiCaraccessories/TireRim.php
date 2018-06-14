637f294492cde222be42e2a192e31f3bbe1636c6
Yodev <natlove.natlove202@gmail.com>
2018-06-14 11:36:48 +0700

    search  rim

---
+++ application/controllers/apiCaraccessories/TireRim.php
@@ -0,0 +1,63 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class TireRim extends BD_Controller {
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        //$this->auth();
+    }
+    function searchrim_post(){
+        $columns = array( 
+            0 => null 
+        );
+        $limit = $this->post('length');
+        $start = $this->post('start');
+        $order = $columns[$this->post('order')[0]['column']];
+        $dir = $this->post('order')[0]['dir'];
+        $this->load->model("rims");
+        $totalData = $this->rims->allrim_count();
+        $totalFiltered = $totalData; 
+        if(empty($this->post('rimName')))
+        {            
+            $posts = $this->rims->allrim($limit,$start,$order,$dir);
+        }
+        else {
+            $search = $this->post('rimName'); 
+            $status = 1;
+            $posts =  $this->rims->rim_search($limit,$start,$search,$order,$dir,$status);
+            $totalFiltered = $this->rims->rim_search_count($search,$status);
+        }
+        $data = array();
+        $index = 0;
+        $count = 0;
+        if(!empty($posts))
+        {
+            foreach ($posts as $post)
+            {
+                $nestedData[$count]['rimId'] = $post->rimId;
+                $nestedData[$count]['rimName'] = $post->rimName;
+                $nestedData[$count]['status'] = $post->status;
+                $data[$index] = $nestedData;
+                if($count >= 3){
+                    $count = -1;
+                    $index++;
+                    $nestedData = [];
+                }
+                
+                $count++;
+                
+            }
+        }
+        $json_data = array(
+            "draw"            => intval($this->post('draw')),  
+            "recordsTotal"    => intval($totalData),  
+            "recordsFiltered" => intval($totalFiltered), 
+            "data"            => $data   
+        );
+        $this->set_response($json_data);
+    }
+
+}
---
+++ application/views/caraccessory/tirerim/content.php
@@ -0,0 +1,40 @@
+<!-- Page wrapper  -->
+<div class="page-wrapper">
+    <!-- Bread crumb -->
+    <div class="row page-titles">
+        <div class="col-md-5 align-self-center">
+            <h3 class="text-primary">รายการขอบยาง</h3>  
+        </div>
+        <div class="col-md-7 align-self-center">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/TireRim"); ?>">รายการขอบยาง</a></li>
+                <li class="breadcrumb-item active">ค้นหา</li>
+            </ol>
+        </div>
+    </div>
+    <!-- End Bread crumb -->
+    <!-- Container fluid  -->
+    <div class="container-fluid">
+        <div class="row bg-white p-30 search-header">
+            <div class="col-md-6">
+                <a href="<?=base_url("caraccessory/car/createcar") ?>">
+                    <button type="button" class="btn btn-info m-b-10 m-l-5" >Create</button>
+                </a>     
+            </div>
+            <div class="col-md-6">
+                <div class="input-group input-group-default">
+                    <input type="text" placeholder="ค้นหา" name="Search" id="brand-search" class="form-control">
+                    <span class="input-group-btn"><button class="btn btn-primary" id="btn-search"><i class="ti-search"></i></button></span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="table">
+            <table class="table table-bordered" id="brand-table" width="100%" cellspacing="0">
+                <thead>
+                    <th></th>
+                </thead>	
+            </table>
+        </div>
+    </div>
+    <!-- End Container fluid  -->
