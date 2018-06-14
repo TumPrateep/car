27493761ea92171a36fe4d478146fd46682f2cca
tumtpk <pk.prateep@gmail.com>
2018-06-14 16:54:32 +0700

    add header

--- application/views/caraccessory/spareUndercarriage/create/content.php
+++ application/views/caraccessory/spareUndercarriage/create/content.php
@@ -15,41 +15,27 @@
     <!-- End Bread crumb -->
     <!-- Container fluid  -->
 
-    <div class="container-fluid">   
+    <div class="container-fluid">     
         <div class="row">
-            <div class="col-12">
-                <div class="card card-outline-success">
-
-                    <div class="card-header">
-                        <h4 class="m-b-0 text-white">เพิ่มรายการอะไหล่</h4>
+            <div class="col-lg-12">
+                <div class="card card-header">
+                    <div class="card-title">
+                        <h4>เพิ่มรายการอะไหล่</h4>
                     </div>
                     <div class="card-body">
-                        <form action="#">
-                            <div class="form-body">
-                                <h3 class="card-title m-t-15">รายละเอียด</h3>
-                                <hr>
-                                <form id="createsparesBrand">
-                                    <input type="hidden" id="spares_undercarriageId" name="spares_undercarriageId" value="<?=$spares_undercarriageId ?>">
-                                        <div class="row p-t-20">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label class="control-label">ชื่ออะไหล่</label>
-                                                    <input type="text" class="form-control" placeholder="ชื่อรายการอะไหล่" name="spares_undercarriageName">
-                                                    <!-- <input type="text" id="firstName" class="form-control" placeholder="ชื่ออะไหล่"> -->
-                                                </div>       
-                                            </div>
-                                    </form> 
+                        <div class="basic-form">
+                            <form>
+                                <div class="form-group col-md-6">
+                                    <label>ชื่อรายการอะไหล่</label>
+                                    <input type="email" class="form-control" placeholder="ชื่อรายการอะไหล่">
                                 </div>
-
-                                <div class="form-actions">
-                                    <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i>บันทึก</button>
-                                    <button type="button" class="btn btn-inverse">ยกเลิก</button>
-                                </div>
-                            </div>
-                        </form>   
+                                <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> บันทึก</button>
+                                <button type="submit" class="btn btn-inverse">ยกเลิก</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>   
+        </div>
     </div>
     <!-- End Container fluid  -->
