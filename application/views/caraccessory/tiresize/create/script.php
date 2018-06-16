8d8fa18aa1a0435c7791b0f2d46c8ecfe6c654f8
Yodev <natlove.natlove202@gmail.com>
2018-06-16 12:50:20 +0700

    validate tiresize

---
+++ application/views/caraccessory/tiresize/create/script.php
@@ -0,0 +1,33 @@
+<script>
+ $("#submit").validate({
+        rules: {
+            tire_size: {
+                required: true
+            },
+            tire_series: {
+                required: true
+            },
+            rim: {
+                required: true
+            }
+        },
+        messages: {
+            tire_size: {
+                required: "กรุณากรอกขนาดยาง"
+            },
+            tire_series: {
+                required: "กรุณากรอกซีรี่ย์ยาง"
+            },
+            rim: {
+                required: "กรุณากรอกขนาดกะทะล้อ"
+            }
+        },
+    });
+    
+    
+    
+   
+</script>
+
+</body>
+</html>
