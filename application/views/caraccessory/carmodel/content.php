c39e1d1870e06b960a82d294b6ecb291287a5471
Yodev <natlove.natlove202@gmail.com>
2018-06-17 14:39:52 +0700

    fix content

--- application/views/caraccessory/carmodel/content.php
+++ application/views/caraccessory/carmodel/content.php
@@ -1,9 +1,10 @@
 <!-- Page wrapper  -->
+
 <div class="page-wrapper">
     <!-- Bread crumb -->
     <div class="row page-titles">
         <div class="col-md-5 align-self-center">
-            <h3 class="text-primary">รายการรุ่นรถ</h3>  
+            <h3 class="text-primary">รุ่นรถ</h3>  
         </div>
         <div class="col-md-7 align-self-center">
             <ol class="breadcrumb">
