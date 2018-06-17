af071acbd953117f7d35dbcd0f179c5f702cd32e
mahdez <mahdee.dark@gmail.com>
2018-06-17 14:42:56 +0700

    edit create ขนาดยาง

--- application/views/caraccessory/tiresize/create/content.php
+++ application/views/caraccessory/tiresize/create/content.php
@@ -19,33 +19,30 @@ Page wrapper  -->
             <div class="card card-outline-success">
                 <div class="card-header">
                     <h4 class="m-b-0 text-white"><i class="fa fa-fw fa-car"></i> เพิ่มข้อมูลขนาดยาง</h4>
-                </div> 
+                </div><br> 
                     <input type="hidden" id="rimId" name="rimId" value="<?=$rimId ?>">
-                        <div class="form-body"> <br>
-                            <div class="row p-t-20">
-                                <div class="col-md-12">
-                                <div class="col">
-                                    <label>ขนาดยาง</label> <span class="error">*</span>
-                                    <input type="text" class="form-control" placeholder="ขนาดยาง" name="tire_size" id="tire_size">
-                                </div>  
-                                <div class="col">
-                                    <label>ซีรี่ย์ยาง</label> <span class="error">*</span>
-                                    <input type="text" class="form-control" placeholder="ซีรี่ย์ยาง" name="tire_series" id="tire_series">
-                                </div>
-                                <div class="col">
-                                    <label>ขนาดกะทะล้อ</label> <span class="error">*</span>
-                                    <input type="text" class="form-control" placeholder="ขนาดกะทะล้อ" name="rim" id="rim">
-                                </div><br>
-                                </div>       
-                            </div>           
-                        </div>
+                        <div class="form-row">
+                            <div class="col">
+                                <label>ขนาดยาง</label> <span class="error">*</span>
+                                <input type="text" class="form-control" placeholder="ขนาดยาง" name="tire_size" id="tire_size">
+                            </div>  
+                            <div class="col">
+                                <label>ซีรี่ย์ยาง</label> <span class="error">*</span>
+                                <input type="text" class="form-control" placeholder="ซีรี่ย์ยาง" name="tire_series" id="tire_series">
+                            </div>
+                            <div class="col">
+                                <label>ขนาดกะทะล้อ</label> <span class="error">*</span>
+                                <input type="text" class="form-control" placeholder="ขนาดกะทะล้อ" name="rim" id="rim">
+                            </div>
+                        </div><br>     
+                          
                         <div class="form-actions">
                             <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> บันทึก</button>
                             <button type="button" class="btn btn-inverse"><i class="fa fa-close"></i> ยกเลิก</button>
                         </div>
                     </form>
                 </div>
-              
+
             </div>
         </div>
     </div>
