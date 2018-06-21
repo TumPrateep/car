0040d8ef5ee2e44870f34d509431318f538b5eeb
Yodev <natlove.natlove202@gmail.com>
2018-06-21 22:50:07 +0700

    update spare undercarriages

--- application/controllers/caraccessory/Spareundercarries.php
+++ application/controllers/caraccessory/Spareundercarries.php
@@ -29,6 +29,17 @@ class Spareundercarries extends CI_Controller {
 		$this->load->view("caraccessory/spareUndercarriage/create/script");
 		
 	}
+	public function updatespareUndercarriage($spares_undercarriageId){
+		$data['spares_undercarriageId'] = $spares_undercarriageId;
+		$this->load->view("caraccessory/layout/head");
+		$this->load->view("caraccessory/layout/header");
+		$this->load->view("caraccessory/layout/left-menu");
+		$this->load->view("caraccessory/spareUndercarriage/update/content",$data);
+		$this->load->view("caraccessory/layout/footer");
+		$this->load->view("caraccessory/layout/foot");
+		$this->load->view("caraccessory/spareUndercarriage/update/script");
+		
+	}
 	
 }
 
