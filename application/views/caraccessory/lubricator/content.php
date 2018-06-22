e6b091e7412733c7aff11defb511cd2735372bb3
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2018-06-22 15:38:14 +0700

    edit น

--- application/views/caraccessory/lubricator/content.php
+++ application/views/caraccessory/lubricator/content.php
@@ -7,7 +7,7 @@
         </div>
         <div class="col-md-7 align-self-center">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/Spareundercarries"); ?>">้ำมันเครื่อง</a></li>
+                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/Spareundercarries"); ?>">น้ำมันเครื่อง</a></li>
                 <li class="breadcrumb-item active">ค้นหา</li>
             </ol>
         </div>
