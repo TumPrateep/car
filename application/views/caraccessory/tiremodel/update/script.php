6d20b4f0c99a012e547c117f6305550f660b2c3b
mahdez <mahdee.dark@gmail.com>
2018-06-22 12:27:31 +0700

    update tiremodel

--- application/views/caraccessory/tiremodel/update/script.php
+++ application/views/caraccessory/tiremodel/update/script.php
@@ -1,5 +1,42 @@
 <script>
+ $("#submit").validate({
+        rules: {
+            tire_modelName: {
+                required: true
+            },
+            
+        },
+        messages: {
+            tire_modelName: {
+                required: "กรุณากรอกรุ่นยาง"
+            },
+           
+    });
+    
+    $("#submit").submit(function(){
+        updateTireModel();
+    })
 
+    function updateTireModel(){
+        event.preventDefault();
+        var isValid = $("#submit").valid();
+        
+        if(isValid){
+            var data = $("#submit").serialize();
+            $.post(base_url+"apiCaraccessories/Tiremodel/updateTireModel",data,
+            function(data){
+                var tire_modelName = $("#tire_modelName").val();
+                if(data.message == 200){
+                    showMessage(data.message,"caraccessory/TireModel/index1/"+value.tire_brandId);
+                }else{
+                    showMessage(data.message,);
+                }
+            });
+            
+        }
+    }
+    
+   
 </script>
 
 </body>
