acf78aaa44003efce57174e12365f9335ed5c0ba
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2018-06-23 15:19:42 +0700

    edit link createSpareBrand

--- application/views/caraccessory/sparebrand/create/script.php
+++ application/views/caraccessory/sparebrand/create/script.php
@@ -25,6 +25,8 @@
             var data = $("#submit").serialize();
             $.post(base_url+"apiCaraccessories/SpareBrand/createSpareBrand",data,
             function(data){
+                var spares_brandName = $("#spares_brandName").val();
+                var spares_undercarriageId = $("#spares_undercarriageId").val();
                 if(data.message == 200){
                     showMessage(data.message,"caraccessory/SpareBrand/index/"+spares_undercarriageId);
                 }else{
