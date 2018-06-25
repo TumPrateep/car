d5b0dd31a4752c67f7b626bfcb1ba1d96b31ef40
mahdez <mahdee.dark@gmail.com>
2018-06-25 14:06:46 +0700

    create update ยี่ห้อน้ำมันเครื่อง

---
+++ application/views/admin/lubricatorbrand/create/content.php
@@ -0,0 +1,47 @@
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="<?=base_url("admin/lubricatorbrand") ?>">ยี่ห้อน้ำมันเครื่อง</a>
+        </li>
+        <li class="breadcrumb-item active">เพิ่มข้อมูล</li>
+      </ol>
+      <!-- Icon Cards-->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-10">
+              <div class="card text-white bg-success">
+                  <div class="card-header">
+
+                    <h3 class="card-title"><i class="fa fa-tint"></i> เพิ่มข้อมูลยี่ห้อน้ำมันเครื่อง</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <!-- form start -->
+                  <form id="update-tiresbrand">
+                  <input type="hidden" id="lubricator_brandId" name="lubricator_brandId" value="<?=$lubricator_brandId?>">
+                    <div class="card-body black bg-light">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label>ชื่อยี่ห้อน้ำมันเครื่่อง</label> <span class="error">*</span>
+                            <input type="text" class="form-control" placeholder="ชื่อยี่ห้อน้ำมันเครื่อง" id="lubricatorbrandName" name="lubricatorbrandName" value="">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group"> 
+                          <input id="tire_brandPicture" type="file" multiple name="tire_brandPicture">
+                      </div>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary ">บันทึก</button>
+                      </div>
+                    </div>
+                    </form>
+                    <!-- /.card-body -->
+                 
+                </div>
+              </div>
+          </div>
+        </div>
+      </section>
+    </div>
--- application/views/admin/lubricatorbrand/update/content.php
+++ application/views/admin/lubricatorbrand/update/content.php
@@ -1,8 +1,8 @@
-    <div class="container-fluid">
+<div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="<?=base_url("admin/tires/tiresbrand") ?>">ยี่ห้อยาง</a>
+          <a href="<?=base_url("admin/lubricatorbrand") ?>">ยี่ห้อน้ำมันเครื่อง</a>
         </li>
         <li class="breadcrumb-item active">เเก้ไขข้อมูล</li>
       </ol>
@@ -14,18 +14,18 @@
               <div class="card text-white bg-success">
                   <div class="card-header">
 
-                    <h3 class="card-title"><i class="fa fa-fw fa-car"></i>เเก้ไขข้อมูลยี่ห้อยาง</h3>
+                    <h3 class="card-title"><i class="fa fa-tint"></i> เเก้ไขข้อมูลยี่ห้อน้ำมันเครื่อง</h3>
                   </div>
                   <!-- /.card-header -->
                   <!-- form start -->
                   <form id="update-tiresbrand">
-                  <input type="hidden" id="tire_brandId" name="tire_brandId" value="<?=$tire_brandId?>">
+                  <input type="hidden" id="lubricator_brandId" name="lubricator_brandId" value="<?=$lubricator_brandId?>">
                     <div class="card-body black bg-light">
                       <div class="row">
                         <div class="col-md-12">
                           <div class="form-group">
-                            <label>ชื่อรุ่น</label> <span class="error">*</span>
-                            <input type="text" class="form-control" placeholder="ชื่อยี่ห้อยางรถยนต์" id="tire_brandName" name="tire_brandName" value="">
+                            <label>ชื่อยี่ห้อน้ำมันเครื่่อง</label> <span class="error">*</span>
+                            <input type="text" class="form-control" placeholder="ชื่อยี่ห้อน้ำมันเครื่อง" id="lubricatorbrandName" name="lubricatorbrandName" value="">
                           </div>
                         </div>
                       </div>
