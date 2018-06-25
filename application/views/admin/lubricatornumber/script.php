e1f32a1b5077418886891f8b201930c58aa18c22
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2018-06-25 19:44:55 +0700

    edit tragets

--- application/views/admin/lubricatornumber/script.php
+++ application/views/admin/lubricatornumber/script.php
@@ -42,7 +42,7 @@
                 {
                     "searchable": false,
                     "orderable": false,
-                    "targets": [0,1,2]
+                    "targets": [0,2]
                 },{
                     "targets": 3,
                     "data": null,
@@ -84,7 +84,8 @@
                 {"className": "dt-center", "targets": [0,1,2,3]},
                 { "width": "10%", "targets": 0 },
                 { "width": "25%", "targets": 1 },
-                { "width": "12%", "targets": 3 }
+                { "width": "10%", "targets": 2 },
+                { "width": "15%", "targets": 3 }
             ]	 
     });
    
