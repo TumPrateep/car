d7022592569bae974d2bd2cb61034c611e6ff5a0
Yodev <natlove.natlove202@gmail.com>
2018-06-25 22:59:18 +0700

    fix

--- application/views/admin/lubricatortype/script.php
+++ application/views/admin/lubricatortype/script.php
@@ -48,11 +48,7 @@
                     "targets": 4,
                     "data": null,
                     "render": function ( data, type, full, meta ) {
-<<<<<<< HEAD
                         return '<a href="'+base_url+"admin/LubricatorType/updatelubricatortype/"+data.lubricator_typeId+'"><button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button></a> '
-=======
-                        return '<a href="'+base_url+"admin/lubricatortype/updatelubricatortype/"+data.lubricator_typeId+'"><button type="button" class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button></a> '
->>>>>>> e1f32a1b5077418886891f8b201930c58aa18c22
                             +'<button type="button" class="delete btn btn-danger" onclick="deleteTireBand('+data.lubricator_brandId+',\''+data.lubricator_brandName+'\')"><i class="fa fa-trash"></i></button>';
                     }
                 },
