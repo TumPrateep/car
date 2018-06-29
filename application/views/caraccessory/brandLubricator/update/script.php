67d0005e223d48e6fc045ab8d12dcbdf023adb27
Yodev <natlove.natlove202@gmail.com>
2018-06-29 20:09:27 +0700

    fix

--- application/views/caraccessory/brandLubricator/update/script.php
+++ application/views/caraccessory/brandLubricator/update/script.php
@@ -26,32 +26,32 @@
         
     });
 
-    // $("#update-lubricatorbrand").submit(function(){
-    //     updatelubricator_brand();
-    // });
+    $("#update-lubricatorbrand").submit(function(){
+        updatelubricator_brand();
+    });
 
-    // function updatelubricator_brand(){
-    //     event.preventDefault();
-    //     var isValid = $("#update-lubricatorbrand").valid();
-    //     if(isValid){
-    //         var myform = document.getElementById("update-lubricatorbrand");
-    //         var formData = new FormData(myform);
-    //         $.ajax({
-    //             url: base_url+"api/Lubricatorbrand/updateLubricatorbrands",
-    //             data: formData,
-    //             processData: false,
-    //             contentType: false,
-    //             type: 'POST',
-    //             success: function (data) {
-    //                 if(data.message == 200){
-    //                     showMessage(data.message,"admin/lubricatorbrand");
-    //                 }else{
-    //                     showMessage(data.message);
-    //                 }
-    //             }
-    //         });
-    //     }
-    // }
+    function updatelubricator_brand(){
+        event.preventDefault();
+        var isValid = $("#update-lubricatorbrand").valid();
+        if(isValid){
+            var myform = document.getElementById("update-lubricatorbrand");
+            var formData = new FormData(myform);
+            $.ajax({
+                url: base_url+"apiCaraccessories/Lubricatorbrand/updateLubricatorbrands",
+                data: formData,
+                processData: false,
+                contentType: false,
+                type: 'POST',
+                success: function (data) {
+                    if(data.message == 200){
+                        showMessage(data.message,"caraccessory/brandLubricator");
+                    }else{
+                        showMessage(data.message);
+                    }
+                }
+            });
+        }
+    }
 </script>
 
 </body>
