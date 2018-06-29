023d940f58a2d66b9ebf38993f96d4537fe9beec
Yodev <natlove.natlove202@gmail.com>
2018-06-29 20:07:48 +0700

    fix

--- application/views/caraccessory/brandLubricator/update/content.php
+++ application/views/caraccessory/brandLubricator/update/content.php
@@ -23,6 +23,7 @@
                     <div class="card-title"></div>
                     <div class="card-body">
                         <form id="update-lubricatorbrand">
+                        <input type="hidden" id="lubricator_brandId" name="lubricator_brandId" value="<?=$lubricator_brandId ?>">
                             <div class="form-body">
                                 <div class="row p-t-20">
                                     <div class="col-md-6">
--- application/views/caraccessory/brandLubricator/update/script.php
+++ application/views/caraccessory/brandLubricator/update/script.php
@@ -11,6 +11,47 @@
             },
         },
     });
+    var lubricator_brandId = $("#lubricator_brandId").val();
+    $.post(base_url+"api/Lubricatorbrand/getLubricatorbrandsById",{
+        "lubricator_brandId": lubricator_brandId
+    },function(data){
+        if(data.message!=200){
+            showMessage(data.message,"caraccessory/brandLubricator");
+        }else{
+            result = data.data;
+            $("#lubricator_brandName").val(result.lubricator_brandName);
+           
+
+        }
+        
+    });
+
+    // $("#update-lubricatorbrand").submit(function(){
+    //     updatelubricator_brand();
+    // });
+
+    // function updatelubricator_brand(){
+    //     event.preventDefault();
+    //     var isValid = $("#update-lubricatorbrand").valid();
+    //     if(isValid){
+    //         var myform = document.getElementById("update-lubricatorbrand");
+    //         var formData = new FormData(myform);
+    //         $.ajax({
+    //             url: base_url+"api/Lubricatorbrand/updateLubricatorbrands",
+    //             data: formData,
+    //             processData: false,
+    //             contentType: false,
+    //             type: 'POST',
+    //             success: function (data) {
+    //                 if(data.message == 200){
+    //                     showMessage(data.message,"admin/lubricatorbrand");
+    //                 }else{
+    //                     showMessage(data.message);
+    //                 }
+    //             }
+    //         });
+    //     }
+    // }
 </script>
 
 </body>
