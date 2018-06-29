8d77d629474b2bab89c04985f434786fa8622410
mahdez <mahdee.dark@gmail.com>
2018-06-29 12:01:31 +0700

    create lubricator

--- application/views/caraccessory/lubricator/create/content.php
+++ application/views/caraccessory/lubricator/create/content.php
@@ -20,7 +20,7 @@
             <div class="col-lg-12">
                 <div class="card card-header">
                     <div class="card-title">
-                        <h4> เพิ่มข้อมูประเภทน้ำมันเครื่อง</h4>
+                        <h4> เพิ่มข้อมูลน้ำมันเครื่อง</h4>
                     </div>
                     <div class="card-body">
                         <div class="basic-form">
@@ -28,17 +28,35 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label class="control-label">ชื่อประเภทน้ำมันเครื่อง</label><span class="error">*</span>
-                                        <input type="text" class="form-control" placeholder="ชื่อประเภทน้ำมันเครื่อง" name="lubricator_typeName" id="lubricator_typeName">
+                                        <label class="control-label">ชื่อน้ำมันเครื่อง</label><span class="error">*</span>
+                                        <input type="text" class="form-control" placeholder="ชื่อน้ำมันเครื่อง" name="lubricator_typeName" id="lubricator_typeName">
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">ระยะทาง</label><span class="error">*</span>
-                                        <input type="text" class="form-control" placeholder="ระยะทาง" name="lubricator_typeSize" id="lubricator_typeSize">
+                                <div class="form-group">
+                                    <div class="input-group input-group-flat">
+                                    <div class="col">
+                                        <label>เบอร์น้ำมันเครื่อง: </label>
+                                        <select class="form-control input-default" name="column" id="column">
+                                            <option value="1" selected></option>
+                                            <option value="2"> </option>
+                                            <option value="3"></option>
+                                        </select>
+                                    </div>
                                     </div>
                                 </div>
-                            </div>
+                                <div class="col-lg-4">
+                                    <div class="input-group input-group-flat">
+                                    <div class="col">
+                                        <label>ประเภทน้ำมันเครื่อง: </label>
+                                        <select class="form-control input-default" name="column" id="column">
+                                            <option value="1" selected></option>
+                                            <option value="2"></option>
+                                            <option value="3"></option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div><br>
                                 <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> บันทึก</button>
                                 <a href="#">
                                 <button type="button" class="btn btn-inverse"><i class="fa fa-close"></i> ยกเลิก</button>
