cc0a6f6a5e126a5fda3871cf12d8fb69ffd3820f
Yodev <natlove.natlove202@gmail.com>
2018-06-29 09:40:47 +0700

    validate create typelubicator

--- application/views/caraccessory/lubricator/create/script.php
+++ application/views/caraccessory/lubricator/create/script.php
@@ -1,5 +1,22 @@
 <script>
-
+    $("#submit").validate({
+        rules: {
+            lubricator_typeName: {
+                required: true
+            },
+            lubricator_typeSize: {
+                required: true
+            }
+        },
+        messages: {
+            lubricator_typeName: {
+                required: "กรุณากรอกชื่อประเภทน้ำมันเครื่อง"
+            },
+            lubricator_typeSize: {
+                required: "กรุณากรอกจำนวนระยะทาง"
+            }
+        },
+    });
 </script>
 
 </body>
