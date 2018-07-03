4ca141fac5d9c5f5c862558c21b85fda105922ee
Yodev <natlove.natlove202@gmail.com>
2018-07-03 11:33:03 +0700

    validate  lu number

--- application/views/caraccessory/numberLubricator/create/script.php
+++ application/views/caraccessory/numberLubricator/create/script.php
@@ -1,5 +1,22 @@
 <script>
-
+$("#submit").validate({
+        rules: {
+            lubricator_number: {
+                required: true
+            },
+            lubricator_gear: {
+                required: true
+            }
+        },
+        messages: {
+            lubricator_number: {
+                required: "กรุณากรอกเบอร์น้ำมันเครื่อง"
+            },
+            lubricator_gear: {
+                required: "กรุณาเลือกประเภทน้ำมันเครื่อง"
+            }
+        },
+    });
 </script>
 
 </body>
