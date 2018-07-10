d446d293d65625aadbdcf5b0f1b745bbf30ec278
Yodev <natlove.natlove202@gmail.com>
2018-07-10 14:01:03 +0700

    update  delete api

--- application/controllers/api/TireChange.php
+++ application/controllers/api/TireChange.php
@@ -41,4 +41,55 @@ class TireChange extends BD_Controller {
             $this->set_response($output, REST_Controller::HTTP_OK);
         }
     }
+    public function update_post(){
+        $tire_front = $this->post('tire_front');
+        $tire_back = $this->post('tire_back');
+        $rimId = $this->post('rimId');
+        $tire_changeId = $this->post('tire_changeId');
+        $userId = $this->session->userdata['logged_in']['id'];
+        $this->load->model('tirechanges');
+        $isDuplicate = $this->tirechanges->checkDuplicateById($tire_changeId,$tire_front,$tire_back,$rimId);
+        if($isDuplicate){
+            $data = array(
+                'tire_changeId' => $tire_changeId,
+                'tire_front' => $tire_front,
+                'tire_back' => $tire_back,
+                'rimId' => $rimId,
+                'update_by' => $userId,
+                'update_at' => date('Y-m-d H:i:s',time()),
+                'status' => 1
+            );
+            $result = $this->tirechanges->update($data);
+            $output["status"] = $result;
+            if($result){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["status"] = false;
+                $output["message"] = REST_Controller::MSG_NOT_UPDATE;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_UPDATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+    public function delete_get(){
+        $tire_changeId = $this->get('tire_changeId');
+        $this->load->model('tirechanges');
+        $checkData = $this->tirechanges->checkData($tire_changeId);
+        if($checkData != null){
+            $checkDelete = $this->tirechanges->delete($tire_changeId);
+            if($checkDelete){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["message"] = REST_Controller::MSG_BE_USED;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_BE_DELETED;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
 }
--- application/models/tirechanges.php
+++ application/models/tirechanges.php
@@ -10,12 +10,36 @@ class tirechanges extends CI_Model{
         if($result > 0){
             return false;
         }else
-        return true;
+            return true;
     }
     function insert($data){
         return $this->db->insert('tire_change',$data);
     }
-
+    function checkDuplicateById($tire_changeId,$tire_front,$tire_back,$rimId){
+        $this->db->select("tire_front,tire_back,rimId");
+        $this->db->from("tire_change");
+        $this->db->where('tire_front',$tire_front);
+        $this->db->where('tire_back',$tire_back);
+        $this->db->where('rimId',$rimId);
+        $this->db->where_not_in('tire_changeId');
+        $result = $this ->db->count_all_results();
+        if($result > 0){
+            return false;
+        }else
+            return true;
+    }
+    function update($data){
+        $this->db->where('tire_changeId',$data['tire_changeId']);
+        $result = $this->db->update('tire_change', $data);
+        return $result;
+    }
+    function checkData($tire_changeId){
+        $this->db->select("tire_front,tire_back,rimId");
+        return $this->db->where('tire_changeId',$tire_changeId)->get("tire_change")->row(); 
+    }
+    function delete($tire_changeId){
+        return $this->db->delete('tire_change', array('tire_changeId' => $tire_changeId));
+    }
 
    
 }
