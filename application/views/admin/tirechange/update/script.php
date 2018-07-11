72fcb98b3bb6b6bc1f703b789ce3e7e7df126950
Yodev <natlove.natlove202@gmail.com>
2018-07-11 15:51:34 +0700

    tirechange

--- application/views/admin/tirechange/update/script.php
+++ application/views/admin/tirechange/update/script.php
@@ -23,4 +23,27 @@
             }
         },
     });
+    var tire_changeId = $("#tire_changeId").val();
+    $("#submit").submit(function(){
+        updatetireChang();
+    })
+
+
+    function updatetireChang(){
+        event.preventDefault();
+        var isValid = $("#submit").valid();
+
+        if(isValid){
+            var data = $("#submit").serialize();
+            $.post(base_url+"api/TireChange/update",data,
+            function(data){
+                if(data.message == 200){
+                    showMessage(data.message,"admin/tires/tirechange");
+                }else{
+                    showMessage(data.message);
+                }
+            });
+            
+        }
+    }
 </script>
