e76781e0aec34ba48e5019bf36df13d47ecc421a
KagamiP <panyapat.test@gmail.com>
2018-07-11 13:09:25 +0700

    update tiresmatching

--- application/controllers/admin/Tires.php
+++ application/controllers/admin/Tires.php
@@ -161,4 +161,15 @@ class Tires extends CI_Controller {
 		$this->load->view("admin/layout/foot");	
 		$this->load->view("admin/tiresmatching/create/script");
 	}
+
+	public function updatetiresmatching($tire_matchingId){
+		$data['tire_matchingId'] = $tire_matchingId;		
+		$this->load->view("admin/layout/head");
+		$this->load->view("admin/layout/left-menu");
+		$this->load->view("admin/layout/header");
+		$this->load->view("admin/tiresmatching/update/content",$data);
+		$this->load->view("admin/layout/footer");
+		$this->load->view("admin/layout/foot");	
+		$this->load->view("admin/tiresmatching/update/script");
+	}
 }
