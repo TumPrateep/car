39564c0cce94686252f4f754b1a316c3b96c1867
Yodev <natlove.natlove202@gmail.com>
2018-07-25 13:52:50 +0700

    delete api

--- application/controllers/api/Modelofcar.php
+++ application/controllers/api/Modelofcar.php
@@ -17,7 +17,7 @@ class Modelofcar extends BD_Controller {
         $isCheck = $this->modelofcars->checkduplicate($modelofcarName,$modelId,$brandId);
         if($isCheck){
             $data = array(
-                'modelofcarId' => $modelofcarId,
+                'modelofcarId' => null,
                 'modelofcarName' => $modelofcarName,
                 'brandId' => $brandId,
                 'modelId' => $modelId,
@@ -38,6 +38,61 @@ class Modelofcar extends BD_Controller {
         }else{
             $output["status"] = false;
             $output["message"] = REST_Controller::MSG_CREATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+    public function update_post(){
+        $brandId = $this->post('brandId');
+        $modelId = $this->post('modelId');
+        $modelofcarName = $this->post('modelofcarName');
+        $userId = $this->session->userdata['logged_in']['id'];
+        $modelofcarId = $this->post('modelofcarId');
+        $this->load->model("modelofcars");
+        $isCheck = $this->modelofcars->checkduplicateforupdate($modelofcarName,$modelId,$brandId,$modelofcarId);
+        if($isCheck){
+            $data = array(
+                'modelofcarId' => $modelofcarId,
+                'brandId' => $brandId,
+                'modelId' => $modelId,
+                'modelofcarName' => $modelofcarName,
+                'status' => 1,
+                'activeFlag' => 1,
+                'update_by' => $userId,
+                'update_at' =>date('Y-m-d H:i:s',time())
+            );
+            $result = $this->modelofcars->update($data,$modelofcarId);
+            $output["status"] = $result;
+            if($result){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["status"] = false;
+                $output["message"] = REST_Controller::MSG_NOT_UPDATE;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+
+        }else{
+            $output["message"] = REST_Controller::MSG_UPDATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
+
+    function delete_get(){
+        $modelofcarId = $this->get('modelofcarId');
+        $this->load->model("modelofcars");
+        $isCheck = $this->modelofcars->Check($modelofcarId);
+        if($isCheck){
+            $result = $this->modelofcars->delete($modelofcarId);
+            if($result){
+                $output["message"] = REST_Controller::MSG_SUCCESS;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }else{
+                $output["message"] = REST_Controller::MSG_BE_USED;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_BE_DELETED;
             $this->set_response($output, REST_Controller::HTTP_OK);
         }
     }
--- application/models/modelofcars.php
+++ application/models/modelofcars.php
@@ -8,11 +8,41 @@ class modelofcars extends CI_Model{
         $this->db->where('brandId',$brandId);
         $result = $this->db->count_all_results();
         if($result > 0 ){
-            return false ;
+            return false;
         }
             return true;
     }
     function insert($data){
         return $this->db->insert('modelofcar',$data);
+    }
+    function checkduplicateforupdate($modelofcarName,$modelId,$brandId,$modelofcarId){
+        $this->db->from('modelofcar');
+        $this->db->where('modelofcarName',$modelofcarName);
+        $this->db->where('modelId',$modelId);
+        $this->db->where('brandId',$brandId);
+        $this->db->where_not_in('modelofcarId',$modelofcarId);
+        $result = $this->db->count_all_results();
+        if($result > 0 ){
+            return false;
+        }
+            return true;
+    }
+
+    function update($data,$modelofcarId){
+        $this->db->where('modelofcarId',$modelofcarId);
+        $result = $this->db->update('modelofcar',$data);
+        return $result;
+    }
+    function Check($modelofcarId){
+        $this->db->from('modelofcar');
+        $this->db->where('modelofcarId',$modelofcarId);
+        $result = $this->db->count_all_results();
+        if($result > 0){
+            return true;
+        }
+            return false;
+    }
+    function delete($modelofcarId){
+        return $this->db->delete('modelofcar',array('modelofcarId' => $modelofcarId));
     }
 }
