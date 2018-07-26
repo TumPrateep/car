dd0ac68db871b050bd1c2d5225ad2e6a91718904
Yodev <natlove.natlove202@gmail.com>
2018-07-26 11:22:31 +0700

    design search

---
+++ application/controllers/caraccessory/Modelofcar.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Modelofcar extends CI_Controller {
+
+	function __construct()
+    {
+        // Construct the parent class
+		parent::__construct();
+		$this->load->view("lib");
+    }
+    function index($brandId,$modelId){
+        $data['brandId'] = $brandId;
+        $data['modelId'] = $modelId;
+        $this->load->view("caraccessory/layout/head");
+		$this->load->view("caraccessory/layout/header");
+		$this->load->view("caraccessory/layout/left-menu");
+		$this->load->view("caraccessory/modelofcar/content",$data);
+		$this->load->view("caraccessory/layout/footer");
+		$this->load->view("caraccessory/layout/foot");
+		$this->load->view("caraccessory/modelofcar/script");
+    }
+    function create($brandId,$modelId){
+        $data['brandId'] = $brandId;
+        $data['modelId'] = $modelId;
+        $this->load->view("caraccessory/layout/head");
+		$this->load->view("caraccessory/layout/header");
+		$this->load->view("caraccessory/layout/left-menu");
+		$this->load->view("caraccessory/modelofcar/create/content",$data);
+		$this->load->view("caraccessory/layout/footer");
+		$this->load->view("caraccessory/layout/foot");
+		$this->load->view("caraccessory/modelofcar/create/script");
+    }
+}
