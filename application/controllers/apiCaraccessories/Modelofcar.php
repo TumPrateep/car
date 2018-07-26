c413a57a7d714a3eb70dceaf9130edb41b243c7e
Yodev <natlove.natlove202@gmail.com>
2018-07-26 10:22:31 +0700

    update api modelofcar

--- application/controllers/apiCaraccessories/Modelofcar.php
+++ application/controllers/apiCaraccessories/Modelofcar.php
@@ -67,4 +67,44 @@ class Modelofcar extends BD_Controller {
             $this->set_response($output, REST_Controller::HTTP_OK);
         }
     }
+    function update_post(){
+        $brandId = $this->post('brandId');
+        $modelId = $this->post('modelId');
+        $modelofcarName = $this->post('modelofcarName');
+        $userId = $this->session->userdata['logged_in']['id'];
+        $modelofcarId = $this->post('modelofcarId');
+        $this->load->model("modelofcars");
+        $isCheck = $this->modelofcars->checkduplicateforupdate($modelofcarName,$modelId,$brandId,$modelofcarId);
+        if($isCheck){
+            $Checkpermistion = $this->modelofcars->Checkpermistion($userId,$modelofcarId);
+            if($Checkpermistion){
+                $data = array(
+                    'modelofcarId' => $modelofcarId,
+                    'brandId' => $brandId,
+                    'modelId' => $modelId,
+                    'modelofcarName' => $modelofcarName,
+                    'status' => 2,
+                    'activeFlag' => 2,
+                    'update_by' => $userId,
+                    'update_at' =>date('Y-m-d H:i:s',time())
+                );
+                $result = $this->modelofcars->update($data,$modelofcarId);
+                $output["status"] = $result;
+                if($result){
+                    $output["message"] = REST_Controller::MSG_SUCCESS;
+                    $this->set_response($output, REST_Controller::HTTP_OK);
+                }else{
+                    $output["status"] = false;
+                    $output["message"] = REST_Controller::MSG_NOT_UPDATE;
+                    $this->set_response($output, REST_Controller::HTTP_OK);
+                }
+            }else{
+                $output["message"] = REST_Controller::MSG_UNAUTHORIZATION;
+                $this->set_response($output, REST_Controller::HTTP_OK);
+            }
+        }else{
+            $output["message"] = REST_Controller::MSG_UPDATE_DUPLICATE;
+            $this->set_response($output, REST_Controller::HTTP_OK);
+        }
+    }
 }
