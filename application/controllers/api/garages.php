b67e8400096f30f74482df7de796e71b303becab
mahdez <mahdee.dark@gmail.com>
2018-07-26 14:07:27 +0700

    add

---
+++ application/controllers/api/garages.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+class garages extends BD_Controller {
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        // $this->auth();
+    }
+
+}
+
