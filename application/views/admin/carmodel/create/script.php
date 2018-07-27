0af8e4640a77549c18eb5613018eaded2078eaec
Yodev <natlove.natlove202@gmail.com>
2018-07-27 11:16:43 +0700

    front create

--- application/views/admin/carmodel/create/script.php
+++ application/views/admin/carmodel/create/script.php
@@ -1,33 +1,41 @@
 <script>
-    $(document).ready(function(){
-
-        $("#submit").submit(function(){
-            createCarmodel();
-        })
-
-        $("#submit").validate({
-            rules: {
-                carModelName: {
-                    required: true
-                },
-            },
-            messages: {
-                carModelName: {
-                    required: "กรุณากรอกชื่อรุ่นรถ"
-                }
-            }
-        });
-
-        function createCarmodel(){
-            event.preventDefault();
-            var isValid = $("#submit").valid();
-            
-            if(isValid){
-                
-                
-            }
+$("#submit").validate({
+    rules: {
+        modelofcarName: {
+            required: true
+        },
+    },
+    messages: {
+        modelofcarName: {
+            required: "กรุณากรอกชื่อรุ่นรถ"
         }
-    });
+    }
+});
+$("#submit").submit(function(){
+        createModelcar();
+    })
+
+
+    function createModelcar(){
+        event.preventDefault();
+        var isValid = $("#submit").valid();
+        
+        if(isValid){
+            var data = $("#submit").serialize();
+            $.post(base_url+"api/Modelofcar/create",data,
+            function(data){
+                var brandId = $("#brandId").val();
+                var modelId = $("#modelId").val();
+                if(data.message == 200){
+                    showMessage(data.message,"admin/car/carmodel/"+brandId+'/'+modelId);
+                }else{
+                    showMessage(data.message);
+                }
+            });
+            
+        }
+    }
+
 </script>
 
 </body>
