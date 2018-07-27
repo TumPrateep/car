821a4df32037856e42e251e776cf9673d25208a8
KagamiP <panyapat.test@gmail.com>
2018-07-27 10:36:18 +0700

    script  carmodel

--- application/views/admin/carmodel/create/script.php
+++ application/views/admin/carmodel/create/script.php
@@ -1,5 +1,33 @@
 <script>
-    
+    $(document).ready(function(){
+
+        $("#submit").submit(function(){
+            createCarmodel();
+        })
+
+        $("#submit").validate({
+            rules: {
+                carModelName: {
+                    required: true
+                },
+            },
+            messages: {
+                carModelName: {
+                    required: "กรุณากรอกชื่อรุ่นรถ"
+                }
+            }
+        });
+
+        function createCarmodel(){
+            event.preventDefault();
+            var isValid = $("#submit").valid();
+            
+            if(isValid){
+                
+                
+            }
+        }
+    });
 </script>
 
 </body>
