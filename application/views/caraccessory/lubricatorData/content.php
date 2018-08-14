c9d1368f2d6e9c3c540b455f0aa12d6a564e4ab5
DESKTOP-G495A6E\moonp <moonpingza1@gmail.com>
2018-08-14 16:44:00 +0700

    add

--- application/views/caraccessory/lubricatorData/content.php
+++ application/views/caraccessory/lubricatorData/content.php
@@ -28,6 +28,57 @@
                 </button>
             </div>
         </div>
-        
+
+        <div class="table mt-20">
+            <table class="table table-bordered" id="brand-table" width="100%" cellspacing="0">
+                <thead>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <tr role="row" class="odd">
+                        <td class="sorting_1">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="card card-header-height">
+
+                                        <div class="card-body">
+                                            <!-- <div class="icon-left"> -->
+                                                <!-- <img class="card-img-top" src="http://localhost/car/public/image/tire_brand/1991f5f7d21e5f4a613089261a791b41.JPG"> -->
+                                                <img class="card-img-top" src="http://localhost/car/public/image/tire/tire.jpg">
+                                            <!-- </div>
+                                            <div class="icon-right">
+                                                <img class="card-img-top" src="http://localhost/car/public/image/icon/Wet-Grip-Tyre-Label.png">
+                                                <img class="card-img-top" src="http://localhost/car/public/image/icon/External-noise-Tyre-Label.png">
+                                                <img class="card-img-top" src="http://localhost/car/public/image/icon/Fuel-efficiency-Tyre-Label.png">
+                                            </div> -->
+                                        </div>
+
+                                        <div class="card-body">
+                                            <div class="text-center">
+                                                <h3>ยี่ห้อน้ำมันเครื่อง/รุ่นน้ำมันเครื่อง</h3>
+                                            </div>
+                                            <div class="text-center">
+                                                <span>รับประกัน 2 ปี</span>
+                                                <span>หรือ</span>
+                                                <span>รับประกัน 1000 กม.</span>
+                                            </div>
+                                            <div class="text-center">
+                                                <h2>xxx,xxx บาท</h2>
+                                            </div>
+                                            <div>
+                                                <a href="#"><button type="button" class="btn btn-warning btn-sm  m-b-10 m-l-5 card-button button-p-helf"><i class="ti-pencil"></i> แก้ไข</button> </a>
+                                                <a href="#"><button type="button" class="btn btn-danger btn-sm  m-b-10 m-l-5 card-button button-p-helf"><i class="ti-trash"></i> ลบ</button> </a>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     </div>
     <!-- End Container fluid  -->
