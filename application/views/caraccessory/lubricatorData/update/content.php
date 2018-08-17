c4f66a07d6d1accb2187da7cad4c1aed90ddedac
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2018-08-17 22:41:00 +0700

    edit

--- application/views/caraccessory/lubricatorData/update/content.php
+++ application/views/caraccessory/lubricatorData/update/content.php
@@ -15,14 +15,27 @@
     <!-- End Bread crumb -->
     <!-- Container fluid  -->
 
-    <div class="container-fluid">   
+<div class="container-fluid">   
         <form id="create-lubricatordata">
         <div class="container-fluid">   
             <div class="row">
                 <div class="col-12">
                     <div class="card card-header-blue">
+                    <input type="hidden" id="lubricator_dataId" name="lubricator_dataId" value="<?=$lubricator_dataId ?>">
                             <div class="form-body"> <br>
                                 <div class="row p-t-20">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label class="control-label">ชนิดน้ำมันเครื่อง</label><span class="error">*</span> <label id="price-error" class="error" for="price"></label>
+                                            <div class="input-group input-group-default">
+                                                <select class="form-control valid" name="lubricator_gear" id="lubricator_gear">
+                                                    <option value="1">น้ำมันเครื่อง</option>
+                                                    <option value="2">น้ำมันเกียร์ธรรมดา</option>
+                                                    <option value="3">น้ำมันเกียร์ออโต</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div> 
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label class="control-label">ยี่ห้อน้ำมันเครื่อง</label><span class="error">*</span> <label id="lubricator_brandId-error" class="error" for="lubricator_brandId"></label>
@@ -42,18 +55,18 @@
                                                 </select>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4">
+                                    </div>     
+                                </div>       
+                                
+                                <div class="row p-t-20">
+                                    <div class="col-md-3">
                                         <div class="form-group">
                                             <label class="control-label">ราคา</label><span class="error">*</span> <label id="price-error" class="error" for="price"></label>
                                             <div class="input-group input-group-default">
                                                 <input type="number" class="form-control" id="price" name="price" placeholder="ราคา">
                                             </div>
                                         </div>
-                                    </div>      
-                                </div>       
-                                
-                                <div class="row p-t-20">
+                                    </div> 
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label class="control-label">การรับประกัน-ปี</label>
--- application/views/caraccessory/lubricatorData/update/script.php
+++ application/views/caraccessory/lubricatorData/update/script.php
@@ -1,161 +1,177 @@
 <script>
+ $("#update-lubricatordata").validate({
+        rules: {
+            lubricator_brandId: {
+                required: true
+            },
+            lubricatorId: {
+                required: true
+            },
+            price: {
+                required: true
+            },
+            tempImage: {
+                required: true
+            }
+        },
+        messages: {
+            lubricator_brandId: {
+                required: "กรุณาเลือกยี่ห้อน้ำมันเครื่อง"
+            },
+            lubricatorId: {
+                required: "กรุณาเลือกรุ่นน้ำมันเครื่อง"
+            },
+            price: {
+                required: "กรุณากรอกราคา"
+            },
+            tempImage: {
+                required: ""
+            }
+        },
+    });
 
-//  $("#update-lubricatordata").validate({
-//         rules: {
-//             lubricator_brandId: {
-//                 required: true
-//             },
-//             lubricatorId: {
-//                 required: true
-//             },
-//             price: {
-//                 required: true
-//             },
-//             tempImage: {
-//                 required: true
-//             }
-//         },
-//         messages: {
-//             lubricator_brandId: {
-//                 required: "กรุณาเลือกยี่ห้อน้ำมันเครื่อง"
-//             },
-//             lubricatorId: {
-//                 required: "กรุณาเลือกรุ่นน้ำมันเครื่อง"
-//             },
-//             price: {
-//                 required: "กรุณากรอกราคา"
-//             },
-//             tempImage: {
-//                 required: ""
-//             }
-//         },
-//     });
-
+    var lubricator_dataId = $("#lubricator_dataId").val();
     
-//     var lubricator_dataId = $("#lubricator_dataId").val();
-    
-//     $.get(base_url+"apiCaraccessories/TireData/getTireData",{
-//         "lubricator_dataId": lubricator_dataId
-//     },function(data){
-//         if(data.message != 200){
-//             showMessage(data.message,"caraccessory/tiredata");
-//         }else{
-//             result = data.data;
-//             console.log(result);
-//             init(result.lubricator_brandId);
-//             $("#price").val(result.price);
-//             $("#warranty_year").val(result.warranty_year);
-//             $("#warranty").val(result.warranty);
-//             $("#warranty_distance").val(result.warranty_distance);
-//             setTirePicture(result.tire_picture);
-//         }
+    $.get(base_url+"apiCaraccessories/LubricatorData/getlubricator_dataId",{
+        "lubricator_dataId": lubricator_dataId
+    },function(data){
+        if(data.message != 200){
+            showMessage(data.message,"caraccessory/lubricatordata");
+        }else{
+            result = data.data;
+            console.log(result);
+            init(result.lubricator_brandId, result.lubricatorId);
+            $("#price").val(result.price);
+            $("#warranty_year").val(result.warranty_year);
+            $("#warranty").val(result.warranty);
+            $("#warranty_distance").val(result.warranty_distance);
+            // setTirePicture(result.tire_picture);
+        }
         
-//     });
+    });
 
-//      function init(lubricator_brandId){
-//         getLubricatorBrand(lubricator_brandId);
-//     }
+    var lubricator_brand = $("#lubricator_brandId");
+    var lubricator_number = $("#lubricator_numberId");
+    
+    function init(lubricator_brandId,lubricator_numberId){
+        getlubricatorBrand(lubricator_brandId, lubricator_numberId);
+    }
+    
+    // function getlubricator_dataId(lubricator_brandId, lubricator_numberId){
+    //     $.get(base_url+"apiCaraccessories/LubricatorData/getlubricator_dataId",{
+    //         function(data){
+    //             var brandData = data.data;
+    //             $.each( brandData, function( key, value ) {
+    //                 tireBrand.append('<option value="' + value.lubricator_brandId + '">' + value.lubricator_brandName + '</option>');
+    //             });
+    //             // lubricator_brand.val(lubricator_brandId);
+    //             // getTireModel(modelId);
+    //         }
+    //     );
+    // }
+        
+    // },function(data){
+    //     var LubricatorData = data.data;
+    //             $("#price").val(LubricatorData.price);
+    //             $("#warranty_year").val(LubricatorData.warranty_year);
+    //             $("#warranty").val(LubricatorData.warranty);
+    //             $("#warranty_distance").val(LubricatorData.warranty_distance);
 
-//     function getLubricatorBrand(lubricator_brandId){
-//         $.get(base_url+"apiCaraccessories/Tirebrand/getAllTireBrand",{},
-//             function(data){
-//                 var brandData = data.data;
-//                 $.each( brandData, function( key, value ) {
-//                     lubricatorBrand.append('<option value="' + value.lubricator_brandId + '">' + value.tire_brandName + '</option>');
-//                 });
-//                 lubricatorBrand.val(brandId);
-//                 getTireModel(modelId);
-//             }
-//         );
-//     }
+    //             $('.image-editor').cropit({
+    //                 allowDragNDrop: false,
+    //                 width: 200,
+    //                 height: 200,
+    //                 type: 'image',
+    //                 imageBackground: true,
+    //                 imageState: {
+    //                     // src: picturePath+"spareundercarriage/"+sparesUndercarriageData.spares_undercarriageDataPicture
+    //                 }
+    //             });
+
+    //             getLubricatorBrand(lubricator_number.lubricator_typeId, lubricator.lubricator_dataId, lubricator.lubricator_brandId, lubricator.lubricator_numberId );
+    //         }
+    //     );
+    // }
+
+    // function init(){
+    //     getlubricator_dataId();
+    // }
+
+    // function getLubricatorBrand(lubricator_brandId){
+    //     $.get(base_url+"apiCaraccessories/Lubricatorbrand/getAllLubricatorBrand",{},
+    //         function(data){
+    //             var brandData = data.data;
+    //             $.each( brandData, function( key, value ) {
+    //                 lubricatorBrand.append('<option value="' + value.lubricator_brandId + '">' + value.lubricator_brandName + '</option>');
+    //             });
+    //             lubricatorBrand.val(brandId);
+    //             // getTireModel(modelId);
+    //         }
+    //     );
+    // }
         
 //     $("#update-lubricatordata").submit(function(){
 //         updateTireData();
 //     })
 
-//     function updateLubricatorData(){
-//         event.preventDefault();
-//         var isValid = $("#update-lubricatordata").valid();
+// //     function updateLubricatorData(){
+// //         event.preventDefault();
+// //         var isValid = $("#update-lubricatordata").valid();
         
-//         if(isValid){
-//             var imageData = $('.image-editor').cropit('export');
-//             $('.hidden-image-data').val(imageData);
-//             var myform = document.getElementById("update-lubricatordata");
-//             var formData = new FormData(myform);
+// //         if(isValid){
+// //             var imageData = $('.image-editor').cropit('export');
+// //             $('.hidden-image-data').val(imageData);
+// //             var myform = document.getElementById("update-lubricatordata");
+// //             var formData = new FormData(myform);
             
-//             $.ajax({
-//                 url: base_url+"apiCaraccessories/Lubricatordata/update",
-//                 data: formData,
-//                 processData: false,
-//                 contentType: false,
-//                 type: 'POST',
-//                 success: function (data) {
-//                     if(data.message == 200){
-//                         showMessage(data.message,"caraccessory/lubricatordata");
-//                     }else{
-//                         showMessage(data.message);
-//                     }
-//                 }
-//             });
-//         }
+// //             $.ajax({
+// //                 url: base_url+"apiCaraccessories/Lubricatordata/update",
+// //                 data: formData,
+// //                 processData: false,
+// //                 contentType: false,
+// //                 type: 'POST',
+// //                 success: function (data) {
+// //                     if(data.message == 200){
+// //                         showMessage(data.message,"caraccessory/lubricatordata");
+// //                     }else{
+// //                         showMessage(data.message);
+// //                     }
+// //                 }
+// //             });
+// //         }
+// //     }
+// var lubricator_brand = $("#lubricator_brandId");
+//     var lubricator = $("#lubricatorId");
+
+//     init();
+
+//     function init(){
+//         getLubracatorBrand();
 //     }
-function getSparesUndercarriageData(){
-        $.get(base_url+"apiCaraccessories/LubricatorData/getlubricator_dataId",{
-            "lubricator_dataId":lubricator_dataId.val()
-        },function(data){
-                var lubricator_data = data.data;
-                $("#price").val(sparesUndercarriageData.price);
-                $("#warranty_year").val(sparesUndercarriageData.warranty_year);
-                $("#warranty").val(sparesUndercarriageData.warranty);
-                $("#warranty_distance").val(sparesUndercarriageData.warranty_distance);
 
-                $('.image-editor').cropit({
-                    allowDragNDrop: false,
-                    width: 200,
-                    height: 200,
-                    type: 'image',
-                    imageBackground: true,
-                    imageState: {
-                        src: picturePath+"spareundercarriage/"+sparesUndercarriageData.spares_undercarriageDataPicture
-                    }
-                });
+//     function getLubracatorBrand(){
+//         $.get(base_url+"apiCaraccessories/Lubricatorbrand/getAllLubricatorBrand",{},
+//             function(data){
+//                 var brandData = data.data;
+//                 $.each( brandData, function( key, value ) {
+//                     lubricator_brand.append('<option value="' + value.lubricator_brandId + '">' + value.lubricator_brandName + '</option>');
+//                 });
+//             }
+//         );
+//     }
 
-                getSparesUndercarriage(sparesUndercarriageData.spares_undercarriageId, sparesUndercarriageData.spares_brandId);
-            }
-        );
-    }
-var lubricator_brand = $("#lubricator_brandId");
-    var lubricator = $("#lubricatorId");
-
-    init();
-
-    function init(){
-        getLubracatorBrand();
-    }
-
-    function getLubracatorBrand(){
-        $.get(base_url+"apiCaraccessories/Lubricatorbrand/getAllLubricatorBrand",{},
-            function(data){
-                var brandData = data.data;
-                $.each( brandData, function( key, value ) {
-                    lubricator_brand.append('<option value="' + value.lubricator_brandId + '">' + value.lubricator_brandName + '</option>');
-                });
-            }
-        );
-    }
-
-    lubricator_brand.change(function(){
-        lubricator.html('<option value="">เลือกรุ่นน้ำมันเครื่อง</option>');
-        $.get(base_url+"apiCaraccessories/Lubricator/getAllLubricator",{
-            lubricator_brandId: $(this).val()
-        },function(data){
-                var lubricatorData = data.data;
-                $.each( lubricatorData, function( key, value ) {
-                    lubricator.append('<option value="' + value.lubricatorId + '">' + value.lubricatorName + " " + value.capacity + " ลิตร " + value.lubricator_number + '</option>');
-                });
-            }
-        );
-    });
+//     lubricator_brand.change(function(){
+//         lubricator.html('<option value="">เลือกรุ่นน้ำมันเครื่อง</option>');
+//         $.get(base_url+"apiCaraccessories/Lubricator/getAllLubricator",{
+//             lubricator_brandId: $(this).val()
+//         },function(data){
+//                 var lubricatorData = data.data;
+//                 $.each( lubricatorData, function( key, value ) {
+//                     lubricator.append('<option value="' + value.lubricatorId + '">' + value.lubricatorName + " " + value.capacity + " ลิตร " + value.lubricator_number + '</option>');
+//                 });
+//             }
+//         );
+//     });
 </script>
 
 </body>
