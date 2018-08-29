e7b09f767559955b025290ce662c40ccf47362fc
Yodev <natlove.natlove202@gmail.com>
2018-08-29 17:43:11 +0700

    create lubricatortypeFormachine api

---
+++ application/controllers/api/LubricatortypeFormachines.php
@@ -0,0 +1,33 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+class lubricatortypeFormachines extends BD_Controller {
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->auth();
+        $this->load->model("lubricatortypeFormachines");
+    }
+    function createlubricatortypeFormachines_post(){
+        $lubricatortypeFormachine = $this->post('lubricatortypeFormachine');
+        $userId = $this->session->userdata['logged_in']['id'];
+        $data_check = $this->model->data_check_create($lubricatortypeFormachine);
+        $data = array(
+            'lubricatortypeFormachineId' => null,
+            'lubricatortypeFormachine' => $lubricatortypeFormachine,
+            'status' => 1,
+            'activeFlag' => 1,
+            'create_at' => date('Y-m-d H:i:s',time()),
+            'create_by' => $userId
+        );
+        $option = [
+            "data_check" => $data_check,
+            "data" => $data,
+            "model" => $this->model,
+            "image_path" => null
+        ];
+
+        $this->set_response(decision_create($option), REST_Controller::HTTP_OK);
+    }
+}
