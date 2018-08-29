46b88cd56afeca91ad55c74092c31cc2fc45053b
Yodev <natlove.natlove202@gmail.com>
2018-08-29 17:47:01 +0700

    update api lubricatortypeFormachines

--- application/controllers/api/LubricatortypeFormachines.php
+++ application/controllers/api/LubricatortypeFormachines.php
@@ -30,4 +30,32 @@ class lubricatortypeFormachines extends BD_Controller {
 
         $this->set_response(decision_create($option), REST_Controller::HTTP_OK);
     }
+
+    function update_post(){
+        $lubricatortypeFormachineId = $this->post('lubricatortypeFormachineId');
+        $lubricatortypeFormachine = $this->post('lubricatortypeFormachine');
+        $userId = $this->session->userdata['logged_in']['id'];
+        $data_check_update = $this->model->getlubricatortypeFormachines($lubricatortypeFormachineId);
+        $data_check = $this->model->wherenot($lubricatortypeFormachineId,$lubricatortypeFormachine);
+        $data = array(
+            'lubricatortypeFormachineId' => $lubricatortypeFormachineId,
+            'lubricatortypeFormachine' => $lubricatortypeFormachine,
+            'status' => 1,
+            'update_at' => date('Y-m-d H:i:s',time()),
+            'update_by' => $userId
+        );
+
+        $option = [
+            "data_check_update" => $data_check_update,
+            "data_check" => $data_check,
+            "data" => $data,
+            "model" => $this->model,
+            "image_path" => null,
+            "old_image_path" => null,
+        ];
+
+        $this->set_response(decision_update($option), REST_Controller::HTTP_OK);
+    }
+
+    }
 }
