0df074ea9ac770eeb4855364d24b8ba2f7c130fb
unknown <sunsun.np@gmail.com>
2018-08-29 18:45:59 +0700

    add backend machinetype

--- application/controllers/api/Machinetype.php
+++ application/controllers/api/Machinetype.php
@@ -10,31 +10,57 @@ class Machinetype extends BD_Controller {
         $this->load->model("machinetypes");
     }
 
-    // function createModel_post(){
+    function create_post(){
 
-    //     $machinetype = $this->post("machinetype");
-    //     $modelofcar_modelofcarId = $this->post("modelofcar_modelofcarId");
-    //     $gear = $this->post("gear");
-    //     $userId = $this->session->userdata['logged_in']['id'];
+        $machinetype = $this->post("machinetype");
+        $modelofcar_modelofcarId = $this->post("modelofcar_modelofcarId");
+        $gear = $this->post("gear");
+        $userId = $this->session->userdata['logged_in']['id'];
 
-    //     $data_check = $this->model->data_check_create($machinetype,$modelofcar_modelofcarId,$gear);
-    //     $data = array(
-    //         'machinetypeId' => null,
-    //         'machinetype' => $machinetype,
-    //         'modelofcar_modelofcarId' => $modelofcar_modelofcarId,
-    //         'gear' => $gear,
-    //         'status' => 1,
-    //         'activeFlag' => 1,
-    //         'create_at' => date('Y-m-d H:i:s',time()),
-    //         'create_by' => $userId
-    //     );
-    //     $option = [
-    //         "data_check" => $data_check,
-    //         "data" => $data,
-    //         "model" => $this->model,
-    //     ];
+        $data_check = $this->model->data_check_create($machinetype,$modelofcar_modelofcarId,$gear);
+        $data = array(
+            'machinetypeId' => null,
+            'machinetype' => $machinetype,
+            'modelofcar_modelofcarId' => $modelofcar_modelofcarId,
+            'gear' => $gear,
+            'status' => 1,
+            'activeFlag' => 1,
+            'create_at' => date('Y-m-d H:i:s',time()),
+            'create_by' => $userId
+        );
+        $option = [
+            "data_check" => $data_check,
+            "data" => $data,
+            "model" => $this->Machinetypes,
+        ];
 
-    //     $this->set_response(decision_create($option), REST_Controller::HTTP_OK);
-    // }
+        $this->set_response(decision_create($option), REST_Controller::HTTP_OK);
+    }
+
+    function changeStatus_post(){
+        $machinetypeId = $this->post('machinetypeId');
+        $status = $this->post("status");
+        if($status == 1){
+            $status = 2;
+        }else{
+            $status = 1;
+        }
+
+        $data_check_update = $this->Machinetypes->getmachinetypebyId($machinetypeId);
+        $data = array(
+            'machinetypeId' => $machinetypeId,
+            'status' => $status,
+            'activeFlag' => 1
+        );
+
+        $option = [
+            "data_check_update" => $data_check_update,
+            "data" => $data,
+            "model" => $this->Machinetypes
+        ];
+
+        $this->set_response(decision_update_status($option), REST_Controller::HTTP_OK);
+    }
+    
 
 }
--- application/models/machinetypes.php
+++ application/models/machinetypes.php
@@ -1,7 +1,7 @@
 
 <?php if(!defined('BASEPATH')) exit('No direct script allowed');
 
-class machinetypes extends CI_Model{
+class Machinetypes extends CI_Model{
     
     function  insert($data){
         return $this->db->insert('machinetype', $data);
@@ -27,6 +27,16 @@ class machinetypes extends CI_Model{
             return true;
         }
         return false;
+    }
+
+    function data_check_create($machinetype,$modelofcar_modelofcarId,$gear){
+        $this->db->select("machinetype");
+        $this->db->from("machinetype");
+        $this->db->where('machinetype', $machinetype);
+        $this->db->where('modelofcar_modelofcarId', $modelofcar_modelofcarId);
+        $this->db->where('gear', $gear);
+        $result = $this->db->get();
+        return $result->row();
     }
 
 }
