3e3a99e5c026a8286ea610714f6cca2bff20fede
Yodev <natlove.natlove202@gmail.com>
2018-08-30 16:55:21 +0700

    re fix

--- application/controllers/api/Lubricatorbrand.php
+++ application/controllers/api/Lubricatorbrand.php
@@ -76,57 +76,50 @@ class Lubricatorbrand extends BD_Controller {
         }
 
         function createLubricatorbrands_post(){
+            $userId = $this->session->userdata['logged_in']['id'];
             $config['upload_path'] = 'public/image/lubricator_brand/';
             $config['allowed_types'] = 'gif|jpg|png';
-            // $config['max_size'] = '100';
-            $config['max_width']  = '1024';
-            $config['max_height']  = '768';
-            $config['overwrite'] = TRUE;
-            $config['encrypt_name'] = TRUE;
-            $config['remove_spaces'] = TRUE;
-    
-            $this->load->library('upload', $config);
+            
             $userId = $this->session->userdata['logged_in']['id'];
-    
-            if ( ! $this->upload->do_upload("lubricator_brandPicture"))
-            {
-                $error = array('error' => $this->upload->display_errors());
-                $output["message"] = REST_Controller::MSG_ERROR;
-                $output["data"] = $error;
+            $img = $this->post('lubricator_brandPicture');
+            $img = str_replace('data:image/png;base64,', '', $img);
+            $img = str_replace(' ', '+', $img);
+            $data = base64_decode($img);
+
+            $imageName = uniqid().'.png';
+            $file = $config['upload_path']. '/'. $imageName;
+            $success = file_put_contents($file, $data);
+
+		if (!$success){
+            $output["message"] = REST_Controller::MSG_ERROR;
+			$this->set_response($output, REST_Controller::HTTP_OK);
+		}else{
+            $lubricator_brandName = $this->post("lubricator_brandName");
+            $isDublicte = $this->lubricatorbrands->checklubricatorbrand($lubricator_brandName);
+            if($isDublicte){
+                $output["message"] = REST_Controller::MSG_CREATE_DUPLICATE;
                 $this->set_response($output, REST_Controller::HTTP_OK);
-            }
-            else
-            {
-                $imageDetailArray = $this->upload->data();
-                $image =  $imageDetailArray['file_name'];
-                $lubricator_brandName = $this->post("lubricator_brandName");
-                $isDublicte = $this->lubricatorbrands->checklubricatorbrand($lubricator_brandName);
-                if($isDublicte){
-                    unlink($config['upload_path'].$image);
-                    $output["message"] = REST_Controller::MSG_CREATE_DUPLICATE;
+            }else{
+                $data = array(
+                    "lubricator_brandId"=> null,
+                    "lubricator_brandPicture"=> $imageName,
+                    "lubricator_brandName"=> $lubricator_brandName,
+                    "status"=> 1,
+                    "create_at" => date('Y-m-d H:i:s',time()),
+                    "create_by" => $userId,
+                    "activeFlag" => 1
+                );
+                $isResult = $this->lubricatorbrands->insert_lubricatorbrand($data);
+                if($isResult){
+                    $output["message"] = REST_Controller::MSG_SUCCESS;
                     $this->set_response($output, REST_Controller::HTTP_OK);
                 }else{
-                    $data = array(
-                        "lubricator_brandId"=> null,
-                        "lubricator_brandPicture"=> $image,
-                        "lubricator_brandName"=> $lubricator_brandName,
-                        "status"=> 1,
-                        "create_at" => date('Y-m-d H:i:s',time()),
-                        "create_by" => $userId,
-                        "activeFlag" => 1
-                    );
-                    $isResult = $this->lubricatorbrands->insert_lubricatorbrand($data);
-                    if($isResult){
-                        $output["message"] = REST_Controller::MSG_SUCCESS;
-                        $this->set_response($output, REST_Controller::HTTP_OK);
-                    }else{
-                        unlink($config['upload_path'].$image);
-                        $output["message"] = REST_Controller::MSG_NOT_CREATE;
-                        $this->set_response($output, REST_Controller::HTTP_OK);
-                    }
+                    $output["message"] = REST_Controller::MSG_NOT_CREATE;
+                    $this->set_response($output, REST_Controller::HTTP_OK);
                 }
             }
-        }
+		}
+    }
 
         function updateLubricatorbrands_post(){
             $config['upload_path'] = 'public/image/lubricator_brand/';
