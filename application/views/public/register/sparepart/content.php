efec2dee4d6a6264ffb0a3850ce91e0d59ca24cf
unknown <sunsun.np@gmail.com>
2018-09-20 15:50:47 +0700

    add input register

--- application/views/public/register/sparepart/content.php
+++ application/views/public/register/sparepart/content.php
@@ -7,176 +7,141 @@
 				<div class="row">
 					<div class="col-md-2">
 						<div class="form-group">
-							<label for="exampleFormControlSelect1">Example select</label>
+							<label for="exampleFormControlSelect1">คำนำหน้า</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>คำนำหน้า</option>
+								<option>นาย</option>
+								<option>นาง</option>
+								<option>นางสาว</option>
 							</select>
 						</div>
 					</div>
 					<div class="col-md-5">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">ชื่อ</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ชื่อ">
 						</div>
 					</div>
 					<div class="col-md-5">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">นามสกุล</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="นามสกุล">
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-12">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+							<label for="exampleFormControlTextarea1">ที่อยู่</label>
+							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ที่อยู่"></textarea>
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">จังหวัด</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>จังหวัด</option>
 							</select>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">อำเภอ</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>อำเภอ</option>
 							</select>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">ตำบล</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>ตำบล</option>
 							</select>
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-4">
-						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
+							<div class="form-group">
+								<label for="exampleFormControlInput1">รหัสไปรษณย์</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="รหัสไปรษณย์">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">เบอร์โทรศัพท์ที่สามารถติดต่อได้</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เบอร์โทรศัพท์">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">เบอร์โทรศัพท์</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เบอร์โทรศัพท์">
+							</div>
 						</div>
 					</div>
-					<div class="col-md-4">
-						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
-						</div>
-					</div>
-				</div>
 				<h4 class="underline">ข้อมูลร้านอะไหล่</h4>
 				<div class="row">
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">ชื่อร้านค้าส่ง</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ชื่อร้านค้าส่ง">
 						</div>
 					</div>
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">หมายเลขทะเบียนการค้า</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="หมายเลขทะเบียนการค้า">
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">ชื่อ</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ชื่อ">
 						</div>
 					</div>
 					<div class="col-md-6">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+							<label for="exampleFormControlInput1">เลขรหัสบัตรประชาชน</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เลขรหัสบัตรประชาชน">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<label for="exampleFormControlTextarea1">ที่อยู่</label>
+							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ที่อยู่"></textarea>
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">จังหวัด</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>จังหวัด</option>
 							</select>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">อำเภอ</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>อำเภอ</option>
 							</select>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
+							<label for="exampleFormControlTextarea1">ตำบล</label>
 							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
+								<option>ตำบล</option>
 							</select>
 						</div>
 					</div>
@@ -184,32 +149,20 @@
 				<div class="row">
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<input class="form-control" type="text">
+							<label for="exampleFormControlTextarea1">รหัสไปรษณย์</label>
+							<input class="form-control" type="text" placeholder="รหัสไปรษณย์">
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
+							<label for="exampleFormControlTextarea1">ละติจูด</label>
+							<input class="form-control" type="text" placeholder="ละติจูด">
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="form-group">
-							<label for="exampleFormControlTextarea1">Example textarea</label>
-							<select class="form-control" id="exampleFormControlSelect1">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
+							<label for="exampleFormControlTextarea1">ลองติจูด</label>
+							<input class="form-control" type="text" placeholder="ลองติจูด">
 						</div>
 					</div>
 				</div>
