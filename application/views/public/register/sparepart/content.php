fd96e3e8ae292b0105cf904eb28dbb111ecc74e5
unknown <bestnacufzxzx1@gmail.com>
2018-09-22 17:14:22 +0700

    edit name

--- application/views/public/register/sparepart/content.php
+++ application/views/public/register/sparepart/content.php
@@ -8,7 +8,7 @@
 			 <div class="row">
 				 <div class="col-md-2">
 					 <div class="form-group">
-						 <label for="user_profile">คำนำหน้า</label>
+						 <label for="user_profile">คำนำหน้า</label><span class="error">*</span>
 						 <select class="form-control" name="titleName" id="titleName">
 							 <option value="">คำนำหน้า</option>
 							 <option value="1">นาย</option>
@@ -26,39 +26,39 @@
 				 <div class="col-md-5">
 					 <div class="form-group">
 						 <label for="user_profile">นามสกุล</label><span class="error">*</span>
-						 <input type="text" class="form-control" id="lastname1" placeholder="นามสกุล">
+						 <input type="text" class="form-control" name="lastname1" id="lastname1" placeholder="นามสกุล">
 					 </div>
 				 </div>
 			 </div>
 			 <div class="row">
 				 <div class="col-md-12">
 					 <div class="form-group">
-						 <label for="user_profile">ที่อยู่</label>
-						 <textarea class="form-control" id="address" rows="3" placeholder="ที่อยู่"></textarea>
+						 <label for="user_profile">ที่อยู่</label><span class="error">*</span>
+						 <textarea class="form-control" name="address" id="address" rows="3" placeholder="ที่อยู่"></textarea>
 					 </div>
 				 </div>
 			 </div>
 			 <div class="row">
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="user_profile">จังหวัด</label>
-						 <select class="form-control" id="provinceId">
+						 <label for="user_profile">จังหวัด</label><span class="error">*</span>
+						 <select class="form-control" name="provinceId" id="provinceId">
 							 <option>จังหวัด</option>
 						 </select>
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="user_profile">อำเภอ</label>
-						 <select class="form-control" id="districtId">
+						 <label for="user_profile">อำเภอ</label><span class="error">*</span>
+						 <select class="form-control" name="districtId" id="districtId">
 							 <option>อำเภอ</option>
 						 </select>
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="user_profile">ตำบล</label>
-						 <select class="form-control" id="subdistrictId">
+						 <label for="user_profile">ตำบล</label><span class="error">*</span>
+						 <select class="form-control" name="subdistrictId" id="subdistrictId">
 							 <option>ตำบล</option>
 						 </select>
 					 </div>
@@ -67,20 +67,20 @@
 			 <div class="row">
 				 <div class="col-md-4">
 						 <div class="form-group">
-							 <label for="user_profile">รหัสไปรษณีย์</label>
-							 <input type="number" class="form-control" id="postCode" placeholder="รหัสไปรษณีย์">
+							 <label for="user_profile">รหัสไปรษณีย์</label><span class="error">*</span>
+							 <input type="number" class="form-control" name="postCode" id="postCode" placeholder="รหัสไปรษณีย์">
 						 </div>
 					 </div>
 					 <div class="col-md-4">
 						 <div class="form-group">
-							 <label for="user_profile">เบอร์โทรศัพท์ที่สามารถติดต่อได้</label>
-							 <input type="number" class="form-control" id="phone1" placeholder="เบอร์โทรศัพท์">
+							 <label for="user_profile">เบอร์โทรศัพท์ที่สามารถติดต่อได้</label><span class="error">*</span>
+							 <input type="number" class="form-control" name="phone1" id="phone1" placeholder="เบอร์โทรศัพท์">
 						 </div>
 					 </div>
 					 <div class="col-md-4">
 						 <div class="form-group">
-							 <label for="user_profile">เบอร์โทรศัพท์</label>
-							 <input type="number" class="form-control" id="phone2" placeholder="เบอร์โทรศัพท์">
+							 <label for="user_profile">เบอร์โทรศัพท์</label><span class="error">*</span>
+							 <input type="number" class="form-control" name="phone2" id="phone2" placeholder="เบอร์โทรศัพท์">
 						 </div>
 					 </div>
 				 </div>
@@ -88,14 +88,14 @@
 			 <div class="row">
 				 <div class="col-md-6">
 					 <div class="form-group">
-						 <label for="car_accessories">ชื่อร้านค้าส่ง</label>
-						 <input type="text" class="form-control" id="car_accessoriesName" placeholder="ชื่อร้านค้าส่ง">
+						 <label for="car_accessories">ชื่อร้านค้าส่ง</label><span class="error">*</span>
+						 <input type="text" class="form-control" name="car_accessoriesName" id="car_accessoriesName" placeholder="ชื่อร้านค้าส่ง">
 					 </div>
 				 </div>
 				 <div class="col-md-6">
 					 <div class="form-group">
-						 <label for="car_accessories">หมายเลขทะเบียนการค้า</label>
-						 <input type="text" class="form-control" id="businessRegistration" placeholder="หมายเลขทะเบียนการค้า">
+						 <label for="car_accessories">หมายเลขทะเบียนการค้า</label><span class="error">*</span>
+						 <input type="text" class="form-control" name="businessRegistration" id="businessRegistration" placeholder="หมายเลขทะเบียนการค้า">
 					 </div>
 				 </div>
 			 </div>
@@ -103,45 +103,45 @@
 				 <div class="col-md-6">
 					 <div class="form-group">
 						 <label for="car_accessories">ชื่อ</label><span class="error">*</span>
-						 <input type="text" class="form-control" id="firstname" placeholder="ชื่อ">
+						 <input type="text" class="form-control" name="firstname" id="firstname" placeholder="ชื่อ">
 					 </div>
 				 </div>
 				 <div class="col-md-6">
 					 <div class="form-group">
-						 <label for="car_accessories">เลขรหัสบัตรประชาชน</label>
-						 <input type="number" class="form-control" id="idcard" placeholder="เลขรหัสบัตรประชาชน">
+						 <label for="car_accessories">เลขรหัสบัตรประชาชน</label><span class="error">*</span>
+						 <input type="number" class="form-control" name="idcard" id="idcard" placeholder="เลขรหัสบัตรประชาชน">
 					 </div>
 				 </div>
 			 </div>
 			 <div class="row">
 				 <div class="col-md-12">
 					 <div class="form-group">
-						 <label for="car_accessories">ที่อยู่</label>
-						 <textarea class="form-control" id="address1" rows="3" placeholder="ที่อยู่"></textarea>
+						 <label for="car_accessories">ที่อยู่</label><span class="error">*</span>
+						 <textarea class="form-control" name="address1" id="address1" rows="3" placeholder="ที่อยู่"></textarea>
 					 </div>
 				 </div>
 			 </div>
 			  <div class="row">
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">จังหวัด</label>
-						 <select class="form-control" id="sparepart_provinceId">
+						 <label for="car_accessories">จังหวัด</label><span class="error">*</span>
+						 <select class="form-control" name="sparepart_provinceId" id="sparepart_provinceId">
 							 <option>จังหวัด</option>
 						 </select>
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">อำเภอ</label>
-						 <select class="form-control" id="sparepart_districtId">
+						 <label for="car_accessories">อำเภอ</label><span class="error">*</span>
+						 <select class="form-control" name="sparepart_districtId" id="sparepart_districtId">
 							 <option>อำเภอ</option>
 						 </select>
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">ตำบล</label>
-						 <select class="form-control" id="sparepart_subdistrictId">
+						 <label for="car_accessories">ตำบล</label><span class="error">*</span>
+						 <select class="form-control" name="sparepart_subdistrictId" id="sparepart_subdistrictId">
 							 <option>ตำบล</option>
 						 </select>
 					 </div>
@@ -150,20 +150,20 @@
 			 <div class="row">
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">รหัสไปรษณีย์</label>
-						 <input class="form-control" id="postCode1" type="text" placeholder="รหัสไปรษณีย์">
+						 <label for="car_accessories">รหัสไปรษณีย์</label><span class="error">*</span>
+						 <input class="form-control" name="postCode1" id="postCode1" type="text" placeholder="รหัสไปรษณีย์">
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">ละติจูด</label>
-						 <input class="form-control" id="latitude" type="text" placeholder="ละติจูด">
+						 <label for="car_accessories">ละติจูด</label><span class="error">*</span>
+						 <input class="form-control" name="latitude" id="latitude" type="text" placeholder="ละติจูด">
 					 </div>
 				 </div>
 				 <div class="col-md-4">
 					 <div class="form-group">
-						 <label for="car_accessories">ลองติจูด</label>
-						 <input class="form-control" id="longitude" type="text" placeholder="ลองติจูด">
+						 <label for="car_accessories">ลองติจูด</label><span class="error">*</span>
+						 <input class="form-control" name="longitude" id="longitude" type="text" placeholder="ลองติจูด">
 					 </div>
 				 </div>
 			 </div>
@@ -171,24 +171,24 @@
 		 <div class="col-md-4 leftline">
 			 <h4 class="users">สมัครลงชื่อเข้าใช้งาน</h4>
 				 <div class="form-group">
-					 <label for="users">ชื่อผู้ใช้งาน</label>
-					 <input type="text" class="form-control" id="username" placeholder="ชื่อผู้ใช้งาน">
+					 <label for="users">ชื่อผู้ใช้งาน</label><span class="error">*</span>
+					 <input type="text" class="form-control" name="username" id="username" placeholder="ชื่อผู้ใช้งาน">
 				 </div>
 				 <div class="form-group">
-					 <label>เบอร์โทรศัพท์</label>
-					 <input type="number" class="form-control" id="phone" placeholder="เบอร์โทรศัพท์">
+					 <label>เบอร์โทรศัพท์</label><span class="error">*</span>
+					 <input type="number" class="form-control" name="phone" id="phone" placeholder="เบอร์โทรศัพท์">
 				 </div>
 				 <div class="form-group">
 					 <label for="users">อีเมล์</label>
-					 <input type="email" class="form-control" id="email" placeholder="อีเมล์">
+					 <input type="email" class="form-control" name="email" id="email" placeholder="อีเมล์">
 				 </div>
 				 <div class="form-group">
-					 <label for="users">รหัสผ่าน</label>
-					 <input type="password" class="form-control" id="password" placeholder="รหัสผ่าน">
+					 <label for="users">รหัสผ่าน</label><span class="error">*</span>
+					 <input type="password" class="form-control" name="password" id="password" placeholder="รหัสผ่าน">
 				 </div>
 				 <div class="form-group">
-					 <label for="users">ยืนยันรหัสผ่าน</label>
-					 <input type="password" class="form-control" id="confirmpassword" placeholder="ยืนยันรหัสผ่าน">
+					 <label for="users">ยืนยันรหัสผ่าน</label><span class="error">*</span>
+					 <input type="password" class="form-control" name="confirmpassword" id="confirmpassword" placeholder="ยืนยันรหัสผ่าน">
 				 </div>
 				 
 				 <button type="submit" class="btn btn-primary btn-block">บันทึก</button>
