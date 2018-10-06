8de0c655b0a45dff588f8e0e7ce0f3b2c2df5ae1
Yodev <natlove.natlove202@gmail.com>
2018-10-06 18:28:43 +0700

    add banner

--- application/views/public/layout/banner.php
+++ application/views/public/layout/banner.php
@@ -4,6 +4,33 @@
 		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="<?=base_url("public/themes/user/");?>images/shop_background.jpg"></div>
 		<div class="home_overlay"></div>
 		<div class="home_content d-flex flex-column align-items-center justify-content-center">
-			<h2 class="home_title">CarJaidee</h2>
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<!-- <ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol> -->
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+					<img class="d-block w-100" src="./public/img/banner/baner.png" alt="First slide">
+					</div>
+					<div class="carousel-item">
+					<img class="d-block w-100" src="./public/img/banner/baner2.png" alt="Second slide">
+					</div>
+					<div class="carousel-item">
+					<img class="d-block w-100" src="./public/img/banner/baner4.png" alt="Third slide">
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+				</div>
+
+		<!-- <img src="<?=base_url('public/img/banner/baner.png'); ?>" > -->
 		</div>
 	</div>
