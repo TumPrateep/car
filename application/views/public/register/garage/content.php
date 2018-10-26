c736de3334c230416e7c5d5014d5df83ca2f2430
moonpingza123 <moonpingza@outlook.com>
2018-10-26 14:49:08 +0700

    edit register garage

--- application/views/public/register/garage/content.php
+++ application/views/public/register/garage/content.php
@@ -51,13 +51,19 @@
 					</div>
 				<h4 class="underline">ข้อมูลอู่ซ่อมรถ</h4>
 				<div class="row">
-					<div class="col-md-6">
+					<div class="col-md-4">
 						<div class="form-group">
 							<label for="exampleFormControlInput1">ชื่ออู่ซ่อมรถ</label>
 							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ชื่ออู่ซ่อมรถ">
 						</div>
 					</div>
-					<div class="col-md-6">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label for="exampleFormControlInput1">เบอร์โทรศัพท์</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เบอร์โทรศัพท์">
+						</div>
+					</div>
+					<div class="col-md-4">
 						<div class="form-group">
 							<label for="exampleFormControlInput1">หมายเลขทะเบียนการค้า</label>
 							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="หมายเลขทะเบียนการค้า">
@@ -65,27 +71,102 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="exampleFormControlInput1">เบอร์โทรศัพท์</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เบอร์โทรศัพท์">
+					<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">จันทร์</label>
+							</div>
 						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="exampleFormControlInput1">เวลาเปิด-ปิด</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="เวลาเปิด-ปิด">
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">อังคาร</label>
+							</div>
 						</div>
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">พุธ</label>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">พฤหัสบดี</label>
+							</div>
+						</div>
+						<!-- <div class="col-md-2">
+							<div class="form-check">
+							  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+							  <label class="form-check-label" for="inlineCheckbox1">อังคาร</label>
+							</div>
+						</div> -->
 					</div>
-				</div>
 				<div class="row">
-					<div class="col-md-12">
-						<div class="form-group">
-							<label for="exampleFormControlTextarea1">ที่อยู่</label>
-							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ที่อยู่"></textarea>
+					<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">ศุกร์</label>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">เสาร์</label>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">อาทิตย์</label>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="form-check">
+							  <label class="form-check-label">
+							  <input class="form-check-input" type="checkbox">ทุกวัน</label>
+							</div>
 						</div>
 					</div>
-				</div>
+				<div class="row">
+					<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">ช่วงเวลาเปิดทำการ</label>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<!-- <label for="exampleFormControlInput1">เลขรหัสบัตรประชาชน</label> -->
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="00 : 00">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<!-- <label for="exampleFormControlInput1">เบอร์โทรศัพท์</label> -->
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="00 : 00">
+							</div>
+						</div>
+					</div>
+				<div class="row">
+					<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">บ้านเลขที่</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="บ้านเลขที่">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">หมู่ที่</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="หมู่ที่">
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">ถนน</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="ถนน">
+							</div>
+						</div>
+					</div>
 				<div class="row">
 					<div class="col-md-4">
 						<div class="form-group">
@@ -113,24 +194,31 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-4">
+					<div class="col-md-3">
 						<div class="form-group">
 							<label for="exampleFormControlTextarea1">รหัสไปรษณีย์</label>
 							<input class="form-control" type="text" placeholder="รหัสไปรษณีย์">
 						</div>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-3">
+						<div class="form-group">
+							<label for="exampleFormControlTextarea1"> </label>
+							<button type="submit" class="btn btn-primary btn-block">เก็บพิกัด</button>
+						</div>
+					</div>
+					<div class="col-md-3">
 						<div class="form-group">
 							<label for="exampleFormControlTextarea1">ละติจูด</label>
 							<input class="form-control" type="text" placeholder="ละติจูด">
 						</div>
 					</div>
-					<div class="col-md-4">
+					<div class="col-md-3">
 						<div class="form-group">
 							<label for="exampleFormControlTextarea1">ลองติจูด</label>
 							<input class="form-control" type="text" placeholder="ลองติจูด">
 						</div>
 					</div>
+					
 				</div>
 			</div>
 			<div class="col-md-4 leftline">
