a20eb696a383521bac4b82c803a9032823b81fbe
unknown <natlove.natlove202@gmail>
2018-11-06 17:11:52 +0700

    create product 1

--- application/views/public/layout/head.php
+++ application/views/public/layout/head.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>plugins/OwlCarousel2-2.2.1/animate.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/cart_styles.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/cart_responsive.css">
+<link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/product_styles.css">
+
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>plugins/jquery-ui-1.12.1.custom/jquery-ui.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/shop_styles.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/shop_responsive.css">
