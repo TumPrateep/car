9d6387ddccc8636b466db8121643bc38f7e6f9db
Sittichai002 <2539sittichai@gmail.com>
2018-11-10 17:44:28 +0700

    add  config mechanic

--- application/views/garage/mechanic/show/content.php
+++ application/views/garage/mechanic/show/content.php
@@ -2,71 +2,67 @@
     <!-- Bread crumb -->
     <div class="row page-titles">
         <div class="col-md-5 align-self-center">
-            <h3 class="text-primary">ดูข้อมูลเพิ่มเติมช่าง</h3> </div>
+            <h3 class="text-primary">ข้อมูลช่างเพิ่มเติม</h3> </div>
         <div class="col-md-7 align-self-center">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="javascript:void(0)">หน้าหลัก</a></li>
                 <li class="breadcrumb-item active">ข้อมูลช่าง</li>
-                <li class="breadcrumb-item active">ดูข้อมูลเพิ่มเติมช่าง</li>
+                <li class="breadcrumb-item active">ข้อมูลช่างเพิ่มเติม</li>
             </ol>
         </div>
     </div>
     <div class="container-fluid">
         <div class="row bg-white m-l-0 m-r-0 box-shadow ">
-            <div class="col-lg-12">
+            <div class="col-lg-10">
+                <div class="card card  bg-secondary">
                 <div class="card">
                     <div class="row"> 
-                        <div class="col-lg-12">
+                        <div class="col-lg-6">
                             <form id="submit">
                             <input type="hidden" id="mechanicId" name="mechanicId" value="<?=$mechanicId ?>">
                                 <div class="row">
                                 <div class="col-lg-4">
                                         <div class="form-group">
-                                            <label for="first_name"><h4>ชื่อ</h4></label>
-                                            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="ชื่อ" title="enter your first name if any.">
+                                            <label for="first_name"><h5>ชื่อ:</h5></label>
+                                            <output type="text"  name="firstName" id="firstName">
                                         </div>
                                     </div>
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-8">
                                         <div class="form-group">
-                                            <label for="last_name"><h4>นามสกุล</h4></label>
-                                            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="นามสกุล" title="enter your last name if any.">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label for="phone"><h4>เลขบัตรประชาชน</h4></label>
-                                            <input type="text" class="form-control" name="personalid" id="personalid" placeholder="เลขบัตรประชาชน" title="enter your phone number if any.">
+                                            <label for="last_name"><h5>นามสกุล:</h5></label>
+                                            <output type="text"  name="lastName" id="lastName">
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-lg-4">
+                                <div class="col-lg-10">
                                         <div class="form-group">
-                                            <label for="phone"><h4>ประสบการณ์(ปี)</h4></label>
-                                            <input type="text" class="form-control" name="exp" id="exp" placeholder=" ปี" title="enter your phone number if any.">
+                                            <label for="phone"><h5>เลขบัตรประชาชน:</h5></label>
+                                            <output type="text"  name="personalid" id="personalid" >
+                                        </div>
+                                    </div>
+                                <div class="col-lg-10">
+                                        <div class="form-group">
+                                            <label for="phone"><h5>เบอร์โทรศัพท์:</h5></label>
+                                            <output type="text"  name="phone" id="phone" >
                                         </div>
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="form-group">
-                                            <label for="phone"><h4>เบอร์โทรศัพท์</h4></label>
-                                            <input type="text" class="form-control" name="phone" id="phone" placeholder="เบอร์โทรศัพท์" title="enter your phone number if any.">
+                                            <label for="mobile"><h5>ความชำนาญ:</h5></label>
+                                            <output type="text" name="skill" id="skill">
                                         </div>
                                     </div>
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label for="mobile"><h4>ความชำนาญ</h4></label>
-                                            <select class="form-control" name="skill" id="skill" placeholder="ความชำนาญ" title="enter your mobile number if any.">
-                                                  <option value="">เลือกความชำนาญ</option>
-                                                  <option value="Honda">Honda</option>
-                                                  <option value="Isuzu">Isuzu</option>
-                                                  <option value="Mazda">Mazda</option> 
-                                                  <option value="Toyota">Toyota</option>
-                                            </select>
+                                            <label for="phone"><h5>ประสบการณ์(ปี):</h5></label>
+                                            <output type="text" name="exp" id="exp" > 
                                         </div>
                                     </div>
+                                
                                 </div>
                                 <div class="row">
-                                    <div class="col-lg text-right">
+                                    <div class="col-lg text-left">
                                         <a class="btn btn-primary create" href="<?=base_url("garage/mechanic/") ?>">
                                              กลับ
                                         </a>
@@ -76,6 +72,10 @@
                         </div>
                     </div>
                 </div>
+                </div>
             </div>
         </div>
     </div>
+
+ 
+
