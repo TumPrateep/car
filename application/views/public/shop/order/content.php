a751b776defe461d887ff10a53f364926e5cda4b
tumtpk <pk.prateep@gmail.com>
2018-12-22 15:57:53 +0700

    edit style

--- application/views/public/shop/order/content.php
+++ application/views/public/shop/order/content.php
@@ -5,16 +5,28 @@
 				<nav aria-label="breadcrumb">
 					<ol class="breadcrumb">
 						<li class="breadcrumb-item"><a href="<?=base_url() ?>">หน้าหลัก</a></li>
+						<li class="breadcrumb-item active" aria-current="page">รายการสั่งซื้อ</li>
 					</ol>
 				</nav>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-lg-3">
-				<div class="shop_sidebar">
-                    <div class="form-group">
-                        <label class="control-label">รายการสั่งซื้อ</label>
-                    </div>
+				<div class="shop_sidebar">	
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">รายการสั่งซื้อ</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Disabled</a>
+						</li>
+					</ul>
 				</div>
             </div>
 			<div class="col-lg-9">
