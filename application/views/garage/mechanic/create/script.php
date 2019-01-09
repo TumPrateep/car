50632dc536c4935d3bfc0741431ad0c3107dae82
kumpiyapol <dometai371419@gmaii.com>
2019-01-09 13:58:28 +0700

    commit  mechanic change and validate

--- application/views/garage/mechanic/create/script.php
+++ application/views/garage/mechanic/create/script.php
@@ -1,47 +1,75 @@
 <script>
+
+         function checkID(id) {
+            if(id.length != 13) return false;
+            for(i=0, sum=0; i < 12; i++)
+                sum += parseFloat(id.charAt(i))*(13-i);
+            if((11-sum%11)%10!=parseFloat(id.charAt(12)))
+                return false;
+            return true;
+        }
+
+        jQuery.validator.addMethod("pid", function(value, element) {
+          return checkID(value);
+        }, 'กรุณากรอกเลขที่บัตรประชาชนให้ถูกต้อง');
+
+
     $(document).ready(function () {
+
         var form = $("#submit");
+
         form.validate({
             rules:{
-                firstname: {
+                firstName: {
                     required: true
                 },
-                lastname: {
+                lastName: {
                     required: true
                 },
                 exp: {
                     required: true
                 },
                 phone: {
-                    required: true
+                    required: true,
+                    minlength: 10,
+                    maxlength: 10
                 },
                 skill: {
                     required: true
+                
                 },
-                personalid:{
-                    required: true
+                personalid: {
+                    required: true,
+                    pid: true
+                   
+                
                 }
             },messages:{
-                firstname: {
+                firstName: {
                     required: "กรุณากรอกชื่อ"
                 },
-                lastname: {
+                lastName: {
                     required: "กรุณากรอกนามสกุล"
                 },
                 exp: {
                     required: "กรุณากรอกประสบการณ์(ปี)"
                 },
                 phone: {
-                    required: "กรุณากรอกเบอร์โทรศัพท์"
+                    required: "กรุณากรอกเบอร์โทรศัพท์",
+                    minlength: "กรุณากรอกเบอร์โทรศัพท์ให้ครบ",
+                    maxlength: "กรุณากรอกเบอร์โทรศัพท์ให้ครบ"
                 },
                 skill: {
                     required: "กรุณาเลือกความชำนาญ"
+                
                 },
-                personalid:{
-                    required: "กรอกหมายเลขประจำตัวประชาชน"
+                personalid: {
+                    required: "กรุณาใส่บัตรประชาชน",
+                    pid: "กรุณากรอกเลขบัตรประชาชนให้ถูกต้อง"
                 }
             }
         });
+
         // form.submit(function (e) { 
         //     e.preventDefault();
         //     var isValid = form.valid();
@@ -51,11 +79,15 @@
         //         alert("unpass");
         //     }
         // });
+
     form.submit(function(){
         createMechanic();
     })
+
+
     function createMechanic(){
         event.preventDefault();
+
         var isValid = form.valid();
         
         if(isValid){
@@ -66,12 +98,14 @@
                 if(data.message == 200){
                     showMessage(data.message,"garage/mechanic");
                 }else{
-                    showMessage(data.message,);
+                    showMessage(data.message);
                 }
+                console.log(data);
             });
         }
     }
     
+
     });
 </script>
 
--- application/views/garage/mechanic/update/script.php
+++ application/views/garage/mechanic/update/script.php
@@ -1,42 +1,74 @@
 <script>
+
+    function checkID(id) {
+            if(id.length != 13) return false;
+            for(i=0, sum=0; i < 12; i++)
+                sum += parseFloat(id.charAt(i))*(13-i);
+            if((11-sum%11)%10!=parseFloat(id.charAt(12)))
+                return false;
+            return true;
+        }
+
+        jQuery.validator.addMethod("pid", function(value, element) {
+          return checkID(value);
+        }, 'กรุณากรอกเลขที่บัตรประชาชนให้ถูกต้อง');
+
+
     $(document).ready(function () {
-        
-        var form = $("#update-member-form"); 
+
+        var form = $("#submit");
+
         form.validate({
             rules:{
-                firstname: {
+                firstName: {
                     required: true
                 },
-                lastname: {
+                lastName: {
                     required: true
                 },
                 exp: {
                     required: true
                 },
                 phone: {
-                    required: true
+                    required: true,
+                    minlength: 10,
+                    maxlength: 10
                 },
                 skill: {
                     required: true
+                },
+                personalid: {
+                    required: true,
+                    pid: true
+                    // minlength: 13,
+                    // maxlength: 13
+                
                 }
             },messages:{
-                firstname: {
+                firstName: {
                     required: "กรุณากรอกชื่อ"
                 },
-                lastname: {
+                lastName: {
                     required: "กรุณากรอกนามสกุล"
                 },
                 exp: {
                     required: "กรุณากรอกประสบการณ์(ปี)"
                 },
                 phone: {
-                    required: "กรุณากรอกเบอร์โทรศัพท์"
+                    required: "กรุณากรอกเบอร์โทรศัพท์",
+                    minlength: "กรุณากรอกเบอร์โทรศัพท์ให้ครบ",
+                    maxlength: "กรุณากรอกเบอร์โทรศัพท์ให้ครบ"
                 },
                 skill: {
                     required: "กรุณาเลือกความชำนาญ"
+                },
+                personalid: {
+                    required: "กรุณาใส่บัตรประชาชน",
+                    pid: "กรุณากรอกเลขบัตรประชาชนให้ถูกต้อง"
                 }
             }
         });
+
         // form.submit(function (e) { 
         //     e.preventDefault();
         //     var isValid = form.valid();
@@ -46,25 +78,35 @@
         //         alert("unpass");
         //     }
         // });
+
     });
+
+
+
         var mechanicId = $("#mechanicId").val();
+
         $.post(base_url+"apiGarage/Mechanic/getMechanic",{
             "mechanicId" : mechanicId
         },function(data){
             if(data.message!=200){
                 showMessage(data.message,"garage/mechanic");
             }
+
             if(data.message == 200){
                 result = data.data;
                 $("#firstName").val(result.firstName);
                 $("#lastName").val(result.lastName);
                 $("#exp").val(result.exp);
                 $("#personalid").val(result.personalid);
+                
                 $("#phone").val(result.phone);
                 $("#skill").val(result.skill);
             }
             
         });
+
+
+
         $("#submit").validate({
                 rules: {
                     firstName: {
@@ -77,9 +119,12 @@
                     }
                 }
         });
+
         $("#submit").submit(function(){
             updatemechanic();
         })
+
+
         function updatemechanic(){
             event.preventDefault();
             var isValid = $("#submit").valid();
@@ -89,7 +134,7 @@
                 $.post(base_url+"apiGarage/Mechanic/updateMechanic",data,
                 function(data){
                     if(data.message == 200){
-                        showMessage(data.message,"garage/mechanic");
+                        showMessage(data.message,"garage/mechanic/");
                     }else{
                         showMessage(data.message);
                     }
@@ -97,6 +142,8 @@
                 
             }
         };
+
+
     
         
 </script>
