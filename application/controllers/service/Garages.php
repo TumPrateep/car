d2099aa659ed1efcd951ef8337a61b1f44b85034
moonpingza123 <moonpingza@outlook.com>
2019-01-11 10:03:31 +0700

    add change page garage

--- application/controllers/service/Garages.php
+++ application/controllers/service/Garages.php
@@ -7,14 +7,82 @@ class Garages extends BD_Controller {
     function __construct()
     {
         parent::__construct();
-        $this->load->model("garage");
+        $this->load->model("searchgarage");
     }
 
-    function getAllGarage_get(){
-        $data = $this->garage->getAllGarage();
-        $this->set_response($data, REST_Controller::HTTP_OK);
-    }
+    // function getAllGarage_get(){
+    //     $data = $this->garage->getAllGarage();
+    //     $this->set_response($data, REST_Controller::HTTP_OK);
+    // }
 
-    
+    public function search_post(){
+        $column = "garageId";
+        $sort = "asc";
+        if($this->post('column') == 3){
+            $column = "status";
+        }else if($this->post('column') == 2){
+            $sort = "desc";
+        }else{
+            $sort = "asc";
+        }
+
+        $limit = $this->post('length');
+        $start = $this->post('start');
+        $order = $columns[$this->post('order')[0]['column']];
+        $dir = $this->post('order')[0]['dir'];
+        $totalData = $this->Searchgarage->allgarage_count();
+        $totalFiltered = $totalData; 
+        // if(empty($this->post('firstName'))&& empty($this->post('skill')))
+        // {            
+            $posts = $this->Searchgarage->allgarage($limit,$start,$order,$dir);
+        // }
+        // else {
+        //     $firstname = $this->post('firstName'); 
+        //     $skill = $this->post('skill');
+        //     $posts =  $this->mechanics->mechanics_search($limit,$start,$order,$dir,$firstname,$skill);
+        //     $totalFiltered = $this->mechanics->mechanics_search_count($firstname,$skill);
+        // }
+        $data = array();
+        if(!empty($posts))
+        {
+            $index = 0;
+            $count = 0;
+            foreach ($posts as $post)
+            {
+                
+                $nestedData[$count]['garageId'] = $post->garageId;
+                $nestedData[$count]['garageName'] = $post->garageName;
+                $nestedData[$count]['businessRegistration'] = $post->businessRegistration;
+                $nestedData[$count]['garageAddress'] = $post->garageAddress;
+                $nestedData[$count]['postCode'] = $post->postCode;
+                $nestedData[$count]['subdistrictId'] = $post->subdistrictId;
+                $nestedData[$count]['districtId'] = $post->districtId;
+                $nestedData[$count]['provinceId'] = $post->provinceId;
+                $nestedData[$count]['latitude'] = $post->latitude;
+                $nestedData[$count]['longtitude'] = $post->longtitude;
+                
+                // $nestedData[$count]['role'] = $post->role;
+                // $nestedData[$count]['exp'] = $post->exp;
+
+
+                $data[$index] = $nestedData;
+                if($count >= 3){
+                    $count = -1;
+                    $index++;
+                    $nestedData = [];
+                }
+                
+                $count++;
+
+            }
+        }
+        $json_data = array(
+            "draw"            => intval($this->post('draw')),  
+            "recordsTotal"    => intval($totalData),  
+            "recordsFiltered" => intval($totalFiltered), 
+            "data"            => $data   
+        );
+        $this->set_response($json_data);
+    }
 
 }
--- application/views/public/menu/searchgarage/content.php
+++ application/views/public/menu/searchgarage/content.php
@@ -1,39 +1,116 @@
 <div class="shop">
 	<div class="container">
-		<h3>ค้นหาอู่ซ่อมรถ</h3>
-		<div class="row">
-			<div class="col-md-12">
-				<div class="row">
-					
-					<div class="col-md-4">
-						<div class="form-group">
-		                    <!-- <label class="control-label">ความเชียวชาญ</label> -->
-		                    <select class="form-control input-default" id="example">
-		                        <option value="">ความเชียวชาญ</option>
-		                        <option value="">รถเบน</option>
-		                        <option value="">รถญี่ปุ่น</option>
-		                        <option value="">รถฮอนด้า</option>
-		                    </select>
-		                </div>
-					</div>
-					<div class="col-md-4">
-						<div class="form-group">
-							<!-- <label for="exampleFormControlTextarea1">จังหวัด</label> -->
-							<select class="form-control" id="example1">
-								<option>จังหวัด</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-md-2">
-						<button type="submit" class="btn btn-info btn-block">ค้นหาอู่</button>
-					</div>
-					<div class="col-md-2">
-						<button type="submit" class="btn btn-warning btn-block">ค้นหาอู่ใกล้เคียง</button>
-					</div>
-				</div>
-				<h4 class="underline"></h4>
-				
+        <div class="row">
+			<div class="col-lg-12">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item"><a href="<?=base_url() ?>">หน้าหลัก</a></li>
+						<li class="breadcrumb-item active" aria-current="page">ค้นหาอูที่ให้บริการ</li>
+					</ol>
+				</nav>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="shop_sidebar">
+                    <div class="form-group">
+                        <label for="garageSearch">จังหวัด</label>
+						<select class="form-control" name="provinceIdSearch" id="provinceIdSearch">
+							<option>จังหวัด</option>
+						</select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="user_profile">อำเภอ</label>
+						<select class="form-control" name="districtIdSearch" id="districtIdSearch">
+							<option>อำเภอ</option>
+						</select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="user_profile">ตำบล</label>
+						<select class="form-control" name="subdistrictIdSearch" id="subdistrictIdSearch">
+							<option>ตำบล</option>
+						</select>
+                    </div>
+
+					<div class="form-group">
+                        <label class="control-label">ยี่ห้อรถ</label>
+                        <select class="form-control input-default" name="brandId" id="brandId">
+                            <option value="">เลือกยี่ห้อรถ</option>
+                        </select>
+					</div>
+                    <div class="form-group">
+                        <label class="control-label">การบริการ</label>
+	                    <select class="form-control input-default" name="Service" id="Service">
+	                        <option value="">เลือกการบริการ</option>
+	                        <option value="">เปลี่ยนน้ำมันเครื่อง</option>
+	                        <option value="">เปลี่ยนอะไหล่ช่วงล่าง</option>
+	                        <option value="">เปลี่ยนยาง</option>
+	                    </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label">ชื่ออู่ซ่อมรถ</label>
+                     	<div class="input-group input-group-default">
+                        	<input type="text" class="form-control" id="garagename" name="garagename" placeholder="ชื่ออู่ซ่อมรถ">
+                    	</div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="control-label">สั่งจองหรือเปลี่ยนทันที</label>
+                        <select class="form-control valid" id="can_change" name="can_change" aria-required="true" aria-invalid="false">
+							<option value="1">เปลี่ยนทันที</option>
+							<option value="2">สั่งจอง</option>
+						</select>
+                    </div>
+                    
+					<br>
+					<div class="col-lg-12 ">
+                        <button type="button" id="btn-search" class="btn-create btn btn-orange btn-block">
+                            <i class="ti-search"> ค้นหา</i>
+                        </button>
+                    </div>
+
+				</div>
+            </div>
+            
+			<div class="col-lg-9">
+				<div class="shop_content">
+					
+					<div class="shop_bar clearfix">
+						<div class="shop_product_count"></div>
+						<div class="shop_sorting">
+							<div class="form-group row">
+								<label for="inputEmail3" class="col-sm-4 col-form-label">จัดเรียง:</label>
+								<div class="col-sm-8">
+									<select class="form-control input-default" name="sort" id="sort">
+										<option value="1" selected>เรียงลำดับจาก ก-ฮ</option>
+										<option value="2">เรียงลำดับจาก ฮ-ก</option>
+										<option value="3">เรียงลำดับจาก สถานะ</option>
+									</select>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="product_grid">
+						<div class="row">
+							<table class="table table-bordered" id="search-table" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th></th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+						
+
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
 	</div>
 </div>
