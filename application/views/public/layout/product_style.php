ec14d7bc0b749313f2e7cec0cc5d041831d1acad
moonpingza123 <moonpingza@outlook.com>
2019-02-23 17:27:16 +0700

    edit carprofie

--- application/views/public/layout/product_style.php
+++ application/views/public/layout/product_style.php
@@ -3,5 +3,15 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/product_responsive.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/main_styles.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/responsive.css">
-
+<style type="text/css">
+	.border-black{
+		color: #000000;
+	    background-color: #ffffff;
+	    border-color: #000000;
+	    border: 3px solid ;
+	    width: 120px;
+	    margin-left: auto;
+  		margin-right: auto;
+	}
+</style>
 </head>
