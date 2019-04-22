167d972b7a8738b41175f7ea40d88456815fcc8d
kumpiyapol <dometai371419@gmaii.com>
2019-04-22 18:42:06 +0700

    commit substring name from mechanic

--- application/views/garage/layout/foot.php
+++ application/views/garage/layout/foot.php
@@ -28,3 +28,14 @@
 
     <script src="<?=base_url("/public/js/jquery-dateformat.min.js") ?>"></script>
     
+
+    <script>
+        function substr(str) {
+            // var str = null;
+            var res = str.substring(0, 13);
+            if(str.length >= 13){
+                res += "...";
+            }
+            return res;
+        }
+    </script>
