927b1a615acaf9f1b6244ce411dc6a79adb4816d
Sittichai002 <2539sittichai@gmail.com>
2019-04-27 19:59:52 +0700

    reserve detail

--- application/models/Reservedetails.php
+++ application/models/Reservedetails.php
@@ -2,15 +2,19 @@
 
 class Reservedetails extends CI_Model{
 
-
-
-    function getReservedetailsById($reserveId){
+    function getReservedetailsById($orderId){
         $this->db->select("reserveId");
         $this->db->where('reserveId',$reserveId);
         $result = $this->db->get("reserve");
         return $result->row();
     }
 
+
+    function gettast($orderId){
+        $this->db->where("orderId", $orderId);
+        $query = $this->db->post("orderdetail");
+        return $query->result();
+    }
 
 
     function allReservedetails($limit,$start,$col,$dir){
@@ -46,22 +50,7 @@ class Reservedetails extends CI_Model{
         $this->db->where('order.status',1);
      
 
-        $this->db->like('rim.rimName',$search);
-        if($status != null){
-            $this->db->where("tire_change_garage.status", $status);
-        }
-        $query = $this->db->limit($limit,$start)
-                ->order_by($col,$dir)
-                ->get();
-                
-        if($query->num_rows()>0)
-        {
-            return $query->result();  
-        }
-        else
-        {
-            return null;
-        }
+       
     }
 
     function Reservedetails_search_count($search,$status){
--- application/views/garage/reservedetail/script.php
+++ application/views/garage/reservedetail/script.php
@@ -1,75 +1,50 @@
 <script>
-    var table = $('#changes-table').DataTable({
-        "language": {
-                "aria": {
-                    "sortAscending": ": activate to sort column ascending",
-                    "sortDescending": ": activate to sort column descending"
-                },
-                "emptyTable": "ไม่พบข้อมูล",
-                "info": "แสดง _START_ ถึง _END_ ของ _TOTAL_ รายการ",
-                "infoEmpty": "ไม่พบข้อมูล",
-                "infoFiltered": "(กรอง 1 จากทั้งหมด _MAX_ รายการ)",
-                "lengthMenu": "_MENU_ รายการ",
-                "zeroRecords": "ไม่พบข้อมูล",
-                "oPaginate": {
-                    "sFirst": "หน้าแรก", // This is the link to the first page
-                    "sPrevious": "ก่อนหน้า", // This is the link to the previous page
-                    "sNext": "ถัดไป", // This is the link to the next page
-                    "sLast": "หน้าสุดท้าย" // This is the link to the last page
-                }
-            },
-            "responsive": true,
-            "bLengthChange": false,
-            "searching": false,
-            "processing": true,
-            "serverSide": true,
-            "ajax":{
-                "url": base_url+"apiGarage/Reservedetail/searchReservedetails",
-                "dataType": "json",
-                "type": "POST",
-                "data": function ( data ) {
-                    data.date = $("#date").val(),
-                    data.reservename = $("#reservename").val(),
-                    data.status = $("#status").val()
-                }
-            },
-            "order": [[ 1, "asc" ]],
-            "columns": [
-                null,
-                null,
-                {"data" : "quantity"},
-            ],
-            "columnDefs": [
-                {
-                    "searchable": false,
-                    "orderable": false,
-                    // "targets": [0,2]
-                },{
-                    "targets": 0,
-                    "data": null,
-                    "render": function ( data, type, full, meta ) {
-                        return meta.row + 1;
-                    }
-                },{
-                    "targets": 1,
-                    "data": null,
-                    "render": function ( data, type, full, meta ) {
-                        var html = "";
-                        var productData = data.data;
-                        var group = data.group;
-                        if(group == "tire"){
-                            html += "ยี่ห้อ "+productData.tire_brandName+" "+"ขนาด "+productData.tire_size+" "+"รุ่น "+productData.tire_modelName;
-                        }else if(group == "lubricator"){
-                            html += productData.lubricator_brandName+" "+productData.lubricatorName+" "+productData.capacity+" ลิตร"+" "+productData.lubricator_number;
-                        }else{
-                            html += productData.spares_undercarriageName+" "+productData.spares_brandName+" "+productData.brandName+" "+productData.modelName+" "+productData.year;
-                        }
-                        return html;
-                    }
-                },
 
-                {"className": "dt-center", "targets": [0,2]}
-            ]	 
-    });
+$(document).ready(function () {
+    var table = $("#showOrder");
+    var orderId = $("#orderId").val();
+    $.get(base_url+"service/Orderdetail/orderDetail?orderId="+orderId, {},
+        function (data, textStatus, jqXHR) {
+            var html = '';
+            $.each(data, function (index, val) { 
+                var picture = "";
+                var content = "";
+                var quantity = "";
 
+                if(val.group == "tire"){
+
+                }else if(val.group == "lubricator"){
+
+                }else if(val.group == "spare"){
+                    picture = base_url+'public/image/spareundercarriage/'+val.picture;
+                    content = val.spares_undercarriageName; 
+                    quantity = val.quantity;
+                }
+
+                // html += '<tr>';
+                //     html += '<td>';
+                //     html += '<img src="'+picture+'" />';
+                //     html += '</td>';
+                //     html += '<td>';
+                //     html += content;
+                //     html += '</td>';
+                //     html += '<td>';
+                //     html += quantity;
+                //     html += '</td>';
+                // html += '</tr>';
+                html += '<tr>'
+                            +'<td><img src="'+picture+'" width="80"/></td>'
+                            +'<td>'+content+'</td>'
+                            +'<td>'+quantity+'</td>'
+                            +'<td>'+currency((val.cost), {  precision: 0 }).format()+' บาท</td>'
+                            +'</tr>';
+            });
+            table.html(html);
+        }
+    );
+});
+   
 </script>
+
+</body>
+</html>
