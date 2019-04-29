4ce17069e857f986bfa13ea1b45cf41950102da1
moonpingza123 <moonpingza@outlook.com>
2019-04-29 20:22:27 +0700

    add view

--- application/views/garage/Orderreceive/content.php
+++ application/views/garage/Orderreceive/content.php
@@ -44,7 +44,7 @@
             <table class="table table-bordered" id="do-table" width="100%" cellspacing="0">
                 <thead>
                   <th><i class="fa fa-sort"></i> ลำดับ</th>
-                  <th><i class="fa fa-picture-o"></i> รูปยี่ห้อรถ</th>
+                  <th><i class="fa fa-picture-o"></i> รูปสินค้า</th>
                   <th><i class="fa fa-rebel"></i>  รายละเอียดสินค้า</th>
                   <th><!-- <i class="fa fa-rebel"></i> -->  จำนวน</th>
                   <th><!-- <i class="fa fa-rebel"></i> -->  ค่าเเรง</th>
