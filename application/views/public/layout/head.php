1c87b2175fe043249626088d1183cf8456b053b8
tumtpk <pk.prateep@gmail.com>
2019-05-28 15:02:50 +0700

    syn cart

--- application/views/public/layout/head.php
+++ application/views/public/layout/head.php
@@ -6,7 +6,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="OneTech shop project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name = "google-signin-client_id" content ="963662169794-4nnkcr8kgj09v1j4sdfm2gto2kths4a0.apps.googleusercontent.com">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/bootstrap4/bootstrap.min.css">
 <link href="<?=base_url("public/themes/user/");?>plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>plugins/OwlCarousel2-2.2.1/owl.carousel.css">
@@ -14,5 +13,5 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>plugins/OwlCarousel2-2.2.1/animate.css">
 <link rel="stylesheet" type="text/css" href="<?=base_url("public/themes/user/");?>styles/jquery-confirm.min.css">
 <link href="<?=base_url("/public/css/loading.css") ?>" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
 
