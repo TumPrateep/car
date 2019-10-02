deb7cc7342e60b60b5e2cfda58dcb226abd74a93
KanashiXI <panyapat.test@gmail.com>
2019-10-02 14:33:31 +0700

    sprade manu&banner

--- application/views/users/layout/menu.php
+++ application/views/users/layout/menu.php
@@ -9,7 +9,7 @@
 				<a href="#search" class="btn btn-main-md width-100p active" id="1">ยางรถยนต์</a>
 			</div>
 			<div class="col-6 col-lg-2">
-				<a href="#search" class="btn btn-main-md width-100p" id="2">น้ำมันเกียร์</a>
+				<a href="<?=base_url("user/Lubicator") ?>" class="btn btn-main-md width-100p" id="2">น้ำมันเกียร์</a>
 			</div>
 			<div class="col-6 col-lg-2">
 				<a href="#search" class="btn btn-main-md width-100p" id="3">โช๊คอัพ</a>
@@ -26,31 +26,3 @@
 		</div>
 	</div>
 </section>
-
-<!--============================
-=            Banner            =
-=============================-->
-
-<section class="banner bg-banner-one overlay">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-6">
-				<!-- Content Block -->
-				<div class="block bg-card">
-					<!-- Coundown Timer -->
-					<h3>บำรุงรักษา&ซ่อมรถ อู่ที่ดีที่สุดใกล้บ้าน</h3>
-					<div>
-						<p class="head"><span class="fa fa-check-circle available"></span> ประหยัดสูงสุด 40% เปรียบเทียบศูนย์บริการ</p>
-						<p class="head"><span class="fa fa-check-circle available"></span> มียางให้เลือกมากกว่า 30 ยี่ห้อ</p>
-						<p class="head"><span class="fa fa-check-circle available"></span> ใช้เวลาเปลี่ยนไม่เกิน 1 ชม.</p>
-						<p class="head"><span class="fa fa-check-circle available"></span> เลือกร้านที่ดีที่สุดใกล้บ้านท่าน</p>
-					</div>
-					<!-- Action Button -->
-					<!-- <a href="#" class="btn btn-white-md">get ticket now</a> -->
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-	
-<!--====  End of Banner  ====-->
