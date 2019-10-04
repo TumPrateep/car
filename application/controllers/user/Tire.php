ca4e1d7377d8d5880164e1bebe12db01275cbb43
KanashiXI <panyapat.test@gmail.com>
2019-10-05 00:16:25 +0700

    Tire's controllers

---
+++ application/controllers/user/Tire.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Tire extends CI_Controller {
+
+	function __construct()
+    {
+        // Construct the parent class
+		parent::__construct();
+		// $this->load->view("lib");
+	}
+	
+	function index(){
+		$this->load->view('users/layout/head');
+		$this->load->view('users/layout/header');
+		$this->load->view('users/layout/menu');
+		// $this->load->view('users/layout/banner');
+		$this->load->view('users/tire/content');
+		$this->load->view('users/layout/footer');
+		$this->load->view('users/layout/foot');
+		$this->load->view('users/tire/script');
+		$this->load->view('users/layout/end');
+    }
+
+}
