88b04c1752f977f7474c3a88addc0cf77a4c6d03
KanashiXI <panyapat.test@gmail.com>
2019-10-10 10:42:08 +0700

    edit file name

--- application/controllers/user/Garage.php
+++ application/controllers/user/Garage.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Caraccessory extends CI_Controller {
+class Garage extends CI_Controller {
 
 	function __construct()
     {
@@ -14,10 +14,10 @@ class Caraccessory extends CI_Controller {
 		$this->load->view('users/layout/header');
 		$this->load->view('users/layout/menu');
 		// $this->load->view('users/layout/banner');
-		$this->load->view('users/caraccessory/content');
+		$this->load->view('users/garage/content');
 		$this->load->view('users/layout/footer');
 		$this->load->view('users/layout/foot');
-		$this->load->view('users/caraccessory/script');
+		$this->load->view('users/garage/script');
 		$this->load->view('users/layout/end');
     }
 
