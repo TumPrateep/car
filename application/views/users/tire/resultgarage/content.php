70bd25379be35e210a39dbd59e43d449839423ca
KanashiXI <panyapat.test@gmail.com>
2019-10-25 22:22:38 +0700

    add content resultgarage

---
+++ application/views/users/tire/resultgarage/content.php
@@ -0,0 +1,62 @@
+<style>
+    .btn-result{
+        padding: 7px 7px 7px 7px;
+    }
+
+</style>
+
+
+
+<section class="section pricing" id="search">
+    <div class="container">
+        <div id="boby">          
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title">
+                        <h3>ผลการค้นหา<span class="alternate">ยางรถยนต์</span></h3> 
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-end">
+                <span class="text"> จัดเรียง:</span>
+                <div class="col-md-2">
+                    <div class="text-right">
+                        <select class="form-control sortby justify-content-end" id="modelofcarId">
+                            <option>ก - ฮ</option>
+                            <option>ฮ - ก</option>
+                            <option>A - Z</option>
+                            <option>Z - A</option>
+                            <option>ราคาต่ำไปสูง</option>
+                            <option>ราคาสูงไปต่ำ</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <br>
+
+            <div class="row row-border">
+                <div class="pic col-md-2 text-center">
+                    <img src="http://www.garagelifethailand.com/wp-content/uploads/2017/03/59_02-Interview-PORSCHE-52.jpg">
+                </div>
+                <div class="detail col-md-3 text-center">
+                    <div class="text"> ที่อยู่ร้าน </div>
+                </div>    
+                <div class="detail col-md-2">
+                    <div class="text"> 20 KM. </div>
+                </div>
+                <div class="detail col-md-2">
+                    <div class="text"> รายละเอียดยาง </div>   
+                </div>
+                <div class="detail col-md-3" >
+                    <div class="row">
+                        <div class="col-md-5">
+                            <span> จำนวน </span>
+                            
+                        </div>
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>            
+</section>
