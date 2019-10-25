d9536662bddb55b4acdf6dad6ff2086de9bf37b8
KanashiXI <panyapat.test@gmail.com>
2019-10-25 22:22:18 +0700

    add controller resultgarage

---
+++ application/controllers/user/Resultgarage.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Resultgarage extends CI_Controller {
+
+
+	function __construct()
+    {
+        // Construct the parent class
+		parent::__construct();
+		// $this->load->view("lib");
+	}
+	function index(){
+		$data = ['tire'=>'active', 'lubricator' => '', 'garage' => ''];
+		$this->load->view('users/layout/head');
+		$this->load->view('users/layout/header');
+		$this->load->view('users/layout/menu', $data);
+		$this->load->view('users/tire/resultgarage/content');
+		$this->load->view('users/layout/footer');
+		$this->load->view('users/layout/foot');
+		$this->load->view('users/layout/end');
+    }
+
+}
