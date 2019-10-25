444590f8a37ded495e95a21792063d51109a126b
KanashiXI <panyapat.test@gmail.com>
2019-10-26 00:08:40 +0700

    update page resultgarage

--- application/views/users/tire/resultgarage/content.php
+++ application/views/users/tire/resultgarage/content.php
@@ -2,7 +2,9 @@
     .btn-result{
         padding: 7px 7px 7px 7px;
     }
-
+    div.address{
+        margin: auto;
+    }
 </style>
 
 
@@ -38,20 +40,35 @@
                 <div class="pic col-md-2 text-center">
                     <img src="http://www.garagelifethailand.com/wp-content/uploads/2017/03/59_02-Interview-PORSCHE-52.jpg">
                 </div>
-                <div class="detail col-md-3 text-center">
-                    <div class="text"> ที่อยู่ร้าน </div>
+                <div class="col-md-3 address">
+                    <p><strong>ปั้มเชลล์ทุ่งแย้</strong></p>
+                    <p>ตำบล/แขวง ในเมือง</p>
+                    <p>อำเภอ/เขต เมืองนครศรีธรรมราช</p>
+                    <p>จังหวัด นครศรีธรรมราช</p>
                 </div>    
                 <div class="detail col-md-2">
                     <div class="text"> 20 KM. </div>
                 </div>
-                <div class="detail col-md-2">
-                    <div class="text"> รายละเอียดยาง </div>   
+                <div class="col-md-2 address"> 
+                    <p><strong>ADVANTA</strong></p>
+                    <p>ER-700</p>
+                    <p>215/55R17 V</p>
+                    <p>Load index: 94 </p>
                 </div>
-                <div class="detail col-md-3" >
+                <div class="detail col-md-3">
                     <div class="row">
-                        <div class="col-md-5">
-                            <span> จำนวน </span>
-                            
+                        <div class="col-5">
+                            <small>จำนวน</small> <input type="number" class="form-control" value="1">
+                        </div>
+                        <div class="col-7">
+                            <small>ราคา</small><h5><span class="alternate">xx,xxx ฿</span></h5> 
+                        </div>
+                        
+                    </div>
+                    <span class="mb-10"></span>
+                    <div class="row">
+                        <div class="col-12">
+                            <button class="btn btn-main-md width-100p bg-orange btn-lg"><i class="fa fa-shopping-bag" aria-hidden="true"></i> สั่งซื้อสินค้า </button>
                         </div>
                     </div>
                 </div>
