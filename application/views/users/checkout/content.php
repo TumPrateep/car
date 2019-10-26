59228020d6891d925a0bae50df93756127b1d597
KanashiXI <panyapat.test@gmail.com>
2019-10-26 20:25:51 +0700

    checkout content complete

--- application/views/users/checkout/content.php
+++ application/views/users/checkout/content.php
@@ -1,15 +1,185 @@
+<style>
+    img.logobank{
+        width:80px;
+        height:80px;
+    }
+    img.extire{
+        width:120px;
+        height:160px;
+    }
+    img.exbrand{
+        width:200px;
+        height:80px;
+    }
+</style>
 <div class="container">
     <div class="container">
         <div id="boby">          
             <div class="row">
                 <div class="col-md-12">
                     <div class="section-title">
-                        <h3>รายละเอียด<span class="alternate">การสั่งสินค้า</span></h3> 
+                        <h3>รายละเอียดสินค้า<span class="alternate"> และ </span>การชำระเงิน</h3> 
                     </div>
                 </div>
             </div>
             <div class="row">
-                
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-12 card">
+                            <div class="card-body"> 
+                                <span>เลือกวันจอง</span>
+                                <button class="btn btn-block">ยืนยัน</button>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <input type="checkbox" name="vehicle1" value="Bike"> ชำระเงินภายหลัง
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-12 card">
+                            <div class="row card-body">
+                                <div class="col-3 text-center">
+                                    <img src="https://i.pinimg.com/474x/00/24/29/002429e4b28532ce5273cafa10be61c2.jpg" class="logobank" alt="">
+                                </div>
+                                <div class="col-1">
+                                </div>
+                                <div class="col-8">
+                                    <span>ธนาคารกสิกรไทย</span><br>
+                                    <span>ชื่อ Carjaidee สาขา ท่าศาลา</span><br>
+                                    <span>11-5586-3598</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h5>รายละเอียดการสั่งสินค้า</h5>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>ชื่อ - นามสกุล</label><span class="error">*</span>
+                                    <input type="text" class="form-control form-control-chosen-required" placeholder="ชื่อ - นามสกุล">
+                                    <label id="firstname-error" class="error" for="firstname"></label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>วัน - เวลา ชำระเงิน</label><span class="error">*</span>
+                                    <input type="text" class="form-control form-control-chosen-required" placeholder="วัน - เวลา ชำระเงิน">
+                                    <label id="firstname-error" class="error" for="firstname"></label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form>
+                                        <div class="form-group">
+                                            <label>รูปหลักฐานการจ่าย</label>
+                                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <a href="<?php echo base_url('search/tire/resultgarage') ?>" class="btn btn-main-md width-100p bg-secondary">ย้อนกลับ</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <a href="" class="btn btn-main-md width-100p bg-orange">ยืนยัน</a>
+                                </div>
+                            </div>
+                            <br>
+                        </div>
+                    </div>    
+                </div>
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-5">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h5>สรุปการสั่งซื้อ</h5>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <span><strong>ศูนย์บริการคาร์ใจดี:</strong></span><br>
+                                    <span>ปั้มเชลล์ทุ่งแย้</span><br>
+                                    <span>ตำบล/แขวง โพธิ์เสด็จ อำเภอ/เขต เมืองนครศรีธรรมราช</span><br>
+                                    <span>จังหวัด นครศรีธรรมราช</span>
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-4">
+                                    <img src="https://www.tyremarket.com/images/products/EP150.jpg" class="extire" alt="">
+                                </div>
+                                <div class="col-8 text-right">
+                                    <img src="<?php echo base_url('public/image/tire_brand/5cc6fb979371e.png') ?>" class="exbrand" alt=""><br>
+                                    <span><strong>Michelin</strong></span><br>
+                                    <span>XCD2</span><br>
+                                    <span>225/75R15</span>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-3">
+                                    <input type="number" class="form-control" value="1">
+                                </div>
+                                <div class="col-9 text-right">
+                                    <h4>3,830 ฿</h4>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-7">
+                                    <span>ยางรถยนต์</span>
+                                </div>
+                                <div class="col-1 text-center">
+                                    <span>1</span>
+                                </div>
+                                <div class="col-4 text-right">
+                                    <span>3630</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-7">
+                                    <span>ค่าจัดส่ง</span>
+                                </div>
+                                <div class="col-1 text-center">
+                                    <span></span>
+                                </div>
+                                <div class="col-4 text-right">
+                                    <span>200</span>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-7">
+                                    <span><strong>รวมทั้งหมด</strong></span>
+                                </div>
+                                <div class="col-1 text-center">
+                                    <span><strong>1</strong></span>
+                                </div>
+                                <div class="col-4 text-right">
+                                    <span><strong>3,860 ฿</strong></span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <hr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>        
