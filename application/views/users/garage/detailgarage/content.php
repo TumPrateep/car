a02b09dbd883c9845b63156df57d22d9cee73404
KanashiXI <panyapat.test@gmail.com>
2019-11-03 15:52:37 +0700

    detailgarage

---
+++ application/views/users/garage/detailgarage/content.php
@@ -0,0 +1,133 @@
+<style>
+    img.pic{
+        width: 220px;
+        height: 180px;
+    }
+    img.icon {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+    }
+    img.thumbnail{
+        width: 60px;
+        height: 60px;
+        cursor: pointer;
+    }
+    p.comment{
+        padding: 15px 0px 10px 0px;
+    }
+    p.reply{
+        padding: 0px 0px 3px 20px;
+        /* margin: 0px 0px 0px 0px; */
+    }
+</style>
+
+<div class="container">
+    <div class="container">
+        <div id="boby">          
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title">
+                        <h3>รายละเอียด<span class="alternate">ศูนย์บริการคาร์ใจดี</span></h3> 
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <img src="<?php echo base_url('public/image/garage/5cef50f1a3d54.png') ?>" class="pic"alt="">                                    
+                                </div>
+                                <div class="col-7">
+                                    <h5>โดมออโต้เซอร์วิส</h5>
+                                    <span><i class="fa fa-calendar" aria-hidden="true"></i> จ, อ, พฤ, ศ, ส, อา</span><br>
+                                    <span><i class="fa fa-clock-o" aria-hidden="true"></i> 08:00 - 17:00 น.</span><br>
+                                    <span><i class="fa fa-phone" aria-hidden="true"></i> 0833969548</span><br>
+                                    <img src="<?=base_url('public/images/icon/wifi.png')?>" class="icon" title="ไวไฟฟรี">
+                                    <img src="<?=base_url('public/images/icon/airconditioner.png')?>" class="icon" title="มีเครื่องปรับอากาศ">
+                                    <img src="<?=base_url('public/images/icon/toilet.png')?>" class="icon" title="มีสุขา">
+                                    <div class="mb-20 text-center">
+                                        <h4>
+                                            <i class="fa fa-star star"></i>
+                                            <i class="fa fa-star star"></i>
+                                            <i class="fa fa-star star"></i>
+                                            <i class="fa fa-star star"></i>
+                                            <i class="fa fa-star star"></i>
+                                        </h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-12">
+                                    <span><strong>ที่อยู่</strong></span><br>
+                                    <span>บ้านเลขที่ 238-1 ตำบล ท่าศาลา อำเภอ ท่าศาลา
+                                    จังหวัด นครศรีธรรมราช 80160</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="col-md-1">
+                </div> -->
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="text-center">
+                                <img src="<?php echo base_url('public/image/role/3.jpg') ?>" alt="">
+                            </p>
+                            <br>
+                            <div class="row">
+                                <div class="col-12">
+                                    <h5>เจ้าของศูนย์บริการ</h5>
+                                    <span>นาย Thanakorn limsakul</span><br>
+                                    <span><i class="fa fa-phone" aria-hidden="true"></i> 0833969552</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    
+                    <br>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-12">
+                    <h4>แสดงความเห็น</h4>
+                </div>                
+            </div>
+            <hr>
+            <div class="row form-group">
+                <div class="col-md-2 text-center">
+                    <img src="<?php echo base_url('public/image/icon/user.png') ?>" class="thumbnail" alt=""><br>
+                    <p><small>User Hoorey</small></p>
+                </div>
+                <div class="col-md-10">
+                    <p class="comment">แสดงความเหนนนนนนนนนนนนนนน</p>
+                    <p class="reply"><strong>ตอบกลับ</strong></p>
+                    <p class="reply">ตอบ 1</p>
+                </div>
+            </div>
+            <hr>
+            <div class="row form-group">
+                <div class="col-md-2 text-center">
+                    <img src="<?php echo base_url('public/image/icon/user.png') ?>" class="thumbnail" alt=""><br>
+                    <p><small>User Hoorey</small></p>
+                </div>
+                <div class="col-md-10">
+                    <p class="comment">แสดงความเหนนนนนนนนนนนนนนน</p>
+                    <p class="reply"><strong>ตอบกลับ</strong></p>
+                    <p class="reply">ตอบ 1</p>
+                </div>
+            </div>
+            <br>          
+        </div>
+    </div>
+</div>       
