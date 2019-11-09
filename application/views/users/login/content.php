9c8189289545eb92c739fbc16c48b62de131a884
KanashiXI <panyapat.test@gmail.com>
2019-11-09 22:41:45 +0700

    register side

--- application/views/users/login/content.php
+++ application/views/users/login/content.php
@@ -30,56 +30,105 @@
         display: none;
     }
 </style>
+<section class="section pricing">
+    <div class="container">
+        <br>
+        <div class="row card flex-row flex-wrap">
+            <div class="col-md-4 card-header border-0 bg-white">
+                <h4 class="text-center">เข้าสู่ระบบ</h4>
+                <div id="error-message" class="alert alert-warning hide" role="alert"></div>
+                <form id="login">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <span>ชื่อผู้ใช้งาน</span>
+                            <input type="text" name="username" class="form-control main" placeholder="ชื่อผู้ใช้งาน">
+                            <span>รหัสผ่าน</span>
+                            <input type="password" name="password" class="form-control main" placeholder="รหัสผ่าน">
+                        </div>    
+                    </div>      
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-main-md width-100p bg-orange" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-sign-in" aria-hidden="true"></i> เข้าสู่ระบบ
+                        </button>
+                    </div>
+                </div>
+                </form>
+                <div class="row no-gutters">
+                    <div class="col-lg-6">
+                        <button class="btn btn-google btn-block" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-google fa-lg" aria-hidden="true"></i> Google
+                        </button>
+                    </div>
+                    <div class="col-lg-6">
+                        <button class="btn btn-fb btn-block" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-facebook-square fa-lg" aria-hidden="true"></i> Facebook
+                        </button>
+                    </div>
+                </div>            
+            </div>
 
-<div class="container">
-    <br>
-    <div class="row card flex-row flex-wrap">
-        <div class="col-md-4 card-header border-0 bg-white">
-            <h4 class="text-center">เข้าสู่ระบบ</h4>
-            <div id="error-message" class="alert alert-warning hide" role="alert"></div>
-            <form id="login">
-            <div class="row">
-                <div class="col-12">
-                    <div class="form-group">
-                        <p class="text-left">ชื่อผู้ใช้งาน</p>
-                        <input type="text" name="username" class="form-control main" placeholder="ชื่อผู้ใช้งาน">
-                        <p class="text-left">รหัสผ่าน</p>
-                        <input type="password" name="password" class="form-control main" placeholder="รหัสผ่าน">
-                    </div>    
-                </div>      
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <button type="submit" class="btn btn-main-md width-100p bg-orange" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-sign-in" aria-hidden="true"></i> เข้าสู่ระบบ
-                    </button>
+            <div class="col-md-8 card-body leftline">
+                <div class="row">
+                    <div class="col-12">
+                        <h5>ลงทะเบียน</h5>
+                    </div>
                 </div>
-            </div>
-            </form>
-            <div class="row no-gutters">
-                <div class="col-lg-6">
-                    <button class="btn btn-google btn-block" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-google fa-lg" aria-hidden="true"></i> Google
-                    </button>
-                </div>
-                <div class="col-lg-6">
-                    <button class="btn btn-fb btn-block" aria-haspopup="true" aria-expanded="false">
-                        <i class="fa fa-facebook-square fa-lg" aria-hidden="true"></i> Facebook
-                    </button>
-                </div>
-            </div>            
-  
-        </div>
-        <div class="col-md-8 card-body leftline">
-            <div class="row">
-                <div class="col-12">
-                    <h6>ลงทะเบียน</h6>
-                </div>
+
+                <form id="register">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>ชื่อ</span>
+                                    <input type="text" name="name" class="form-control main" placeholder="ชื่อ">
+                                </div>
+                                <div class="col-md-6">
+                                    <span>นามสกุล</span>
+                                    <input type="text" name="lastname" class="form-control main" placeholder="นามสกุล">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>เบอร์โทรศัพท์</span>
+                                    <input type="text" name="phoneNumber" class="form-control main" placeholder="เบอร์โทรศัพท์">
+                                </div>
+                                <div class="col-md-6">
+                                    <span>อีเมล์</span>
+                                    <input type="email" name="email" class="form-control main" placeholder="example@Carjaidee.com">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>ชื่อผู้ใช้งาน</span>
+                                    <input type="text" name="username" class="form-control main" placeholder="ชื่อผู้ใช้งาน">
+                                </div>
+                                <div class="col-md-6">
+                                    <span>รหัสผ่าน</span>
+                                    <input type="text" name="password" class="form-control main" placeholder="รหัสผ่าน">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>รหัสผ่านอีกครั้ง</span>
+                                    <input type="text" name="confirmPassword" class="form-control main btn-ga" placeholder="รหัสผ่านอีกครั้ง">
+                                </div>
+                                <div class="col-md-6">
+                                    <button type="submit" class="btn btn-main-md width-100p bg-orange" aria-haspopup="true" aria-expanded="false">
+                                        ลงทะเบียน
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </form>
                 
             </div>
-            
         </div>
+        <br>
     </div>
-    <br>
+</section>
     
-</div>    
