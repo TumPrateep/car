4646133de1c9692a17968c1d4572685e38236c24
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2019-11-09 16:44:06 +0700

    design  user/servicehistory

--- application/views/users/service-history/content.php
+++ application/views/users/service-history/content.php
@@ -1,9 +1,147 @@
 
-<div class="container">
-<br>
+<div class="container">`
     <div class="row flex-row flex-wrap">
         <div class="col-md-12">
-
+            <section class="schedule">
+                <div class="section-title">
+                    <h3>ประวัติการ<span class="alternate" id="title">ซ่อมรถ</span></h3>
+                    <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusm tempor incididunt ut labore</p> -->
+                </div>
+                <div class="schedule-tab">
+                    <ul class="nav nav-pills text-center">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#nov20" data-toggle="pill">
+                            ยาง
+                            <!-- <span>20 November 2017</span> -->
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nov21" data-toggle="pill">
+                            น้ำมันเครื่อง
+                            <!-- <span>21 November 2017</span> -->
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nov22" data-toggle="pill">
+                            อะไหล่
+                            <!-- <span>22 November 2017</span> -->
+                        </a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="schedule-contents">
+                    <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active schedule-item" id="nov20">
+                        <!-- Headings -->
+                        <ul class="m-0 p-0">
+                            <li class="headings">
+                                <div class="time">ลำดับ</div>
+                                <div class="speaker">คำสั่งซื้อ</div>
+                                <div class="subject">สินค้าที่ซ่อม</div>
+                                <div class="venue">วันที่และเวลาเข้าซ่อม</div>
+                            </li>
+                            <!-- Schedule Details -->
+                            <li class="schedule-details">
+                                <div class="block">
+                                    <!-- time -->
+                                    <div class="time">
+                                        1
+                                    </div>
+                                    <!-- Speaker -->
+                                    <div class="speaker">
+                                            <a href="<?=base_url("/user/order/orderdetails") ?>">
+                                                คำสั่งซื้อ 10010 
+                                            </a>
+                                    </div>
+                                    <!-- Subject -->
+                                    <div class="subject">
+                                        bridgestone 255/65r17
+                                    </div>
+                                    <!-- Venue -->
+                                    <div class="venue">
+                                        <i class="fa fa-clock-o"></i>
+                                        <span class="time">05/06/2019</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <!-- Schedule Details -->
+                        </ul>
+                    </div>
+                    <div class="tab-pane fade schedule-item" id="nov21">
+                        <!-- Headings -->
+                        <ul class="m-0 p-0">
+                            <li class="headings">
+                                <div class="time">ลำดับ</div>
+                                <div class="speaker">คำสั่งซื้อ</div>
+                                <div class="subject">สินค้าที่ซ่อม</div>
+                                <div class="venue">วันที่และเวลาเข้าซ่อม</div>
+                            </li>
+                            <!-- Schedule Details -->
+                            <li class="schedule-details">
+                                <div class="block">
+                                    <!-- time -->
+                                    <div class="time">
+                                        1
+                                    </div>
+                                    <!-- Speaker -->
+                                    <div class="speaker">
+                                        <a href="<?=base_url() ?>">
+                                            คำสั่งซื้อ 10010 
+                                        </a>
+                                    </div>
+                                    <!-- Subject -->
+                                    <div class="subject">
+                                        zic x7
+                                    </div>
+                                    <!-- Venue -->
+                                    <div class="venue">
+                                        <i class="fa fa-clock-o"></i>
+                                        <span class="time">06/08/2019</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <!-- Schedule Details -->
+                        </ul>
+                    </div>
+                    <div class="tab-pane fade schedule-item" id="nov22">
+                        <!-- Headings -->
+                        <ul class="m-0 p-0">
+                            <li class="headings">
+                                <div class="time">ลำดับ</div>
+                                <div class="speaker">คำสั่งซื้อ</div>
+                                <div class="subject">สินค้าที่ซ่อม</div>
+                                <div class="venue">วันที่และเวลาเข้าซ่อม</div>
+                            </li>
+                            <!-- Schedule Details -->
+                            <li class="schedule-details">
+                                <div class="block">
+                                    <!-- time -->
+                                    <div class="time">
+                                        1
+                                    </div>
+                                    <!-- Speaker -->
+                                    <div class="speaker">
+                                            <a href="<?=base_url() ?>">
+                                                คำสั่งซื้อ 10010 
+                                            </a>
+                                    </div>
+                                    <!-- Subject -->
+                                    <div class="subject">
+                                        โช้คหน้าคู่ TRW
+                                    </div>
+                                    <!-- Venue -->
+                                    <div class="venue">
+                                        <i class="fa fa-clock-o"></i>
+                                        <span class="time">05/06/2019</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <!-- Schedule Details -->
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
-</div>    
+</div>
