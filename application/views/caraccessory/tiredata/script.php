68f5d4b2cce8a622e10c03819dbed29c49262776
tumtpk <pk.prateep@gmail.com>
2019-12-20 21:56:08 +0700

    add button edit tire

--- application/views/caraccessory/tiredata/script.php
+++ application/views/caraccessory/tiredata/script.php
@@ -1,96 +1,116 @@
 <script>
-    var table = $('#brand-table').DataTable({
-        "language": {
-                "aria": {
-                    "sortAscending": ": activate to sort column ascending",
-                    "sortDescending": ": activate to sort column descending"
-                },
-                "sProcessing":   "กำลังดำเนินการ...",
-                "emptyTable": "ไม่พบข้อมูล",
-                "info": "แสดง _START_ ถึง _END_ ของ _TOTAL_ รายการ",
-                "infoEmpty": "ไม่พบข้อมูล",
-                "infoFiltered": "(กรองข้อมูล _MAX_ ทุกแถว)",
-                "lengthMenu": "_MENU_ รายการ",
-                "zeroRecords": "ไม่พบข้อมูล",
-                "oPaginate": {
-                    "sFirst": "หน้าแรก", // This is the link to the first page
-                    "sPrevious": "ก่อนหน้า", // This is the link to the previous page
-                    "sNext": "ถัดไป", // This is the link to the next page
-                    "sLast": "หน้าสุดท้าย" // This is the link to the last page
-                }
-            },
-            "responsive": true,
-            "bLengthChange": false,
-            "searching": false,
-            "processing": true,
-            "serverSide": true,
-            "orderable": false,
-            "pageLength": 10,
-            "ajax":{
-                "url": base_url+"apicaraccessories/Tiredata/search",
-                "dataType": "json",
-                "type": "POST",
-                "data": function ( data ) {
-                    data.tire_brandId = $("#tire_brandId").val();
-                    data.tire_modelId = $("#tire_modelId").val();
-                    data.rimId = $("#rimId").val();
-                    data.tire_sizeId = $("#tire_sizeId").val();
-                    data.price = $("#price").val();
-                    data.can_change = $("#can_change").val();
-                    data.sort = $("#sort").val();
-                }
-            },
-            "order": [[ 1, "asc" ]],
-            "columns": [
-                null,
-                {"data":"tire_brandName"},
-                {"data":"tire_modelName"},
-                {"data":"tire_size"},
-                null
-            ],
-            "columnDefs": [
-                {
-                    "searchable": false,
-                    "orderable": false,
-                    "targets": [0]
-                },
-                {
-                    "targets": 0,
-                    "data": null,
-                    "render": function ( data, type, full, meta ) {
-                        return meta.row + 1;
-                    }
-                },
-                {
-                    "targets": 4,
-                    "data": null,
-                    "render": function ( data, type, full, meta ) {
-                        return currency(data.price, { precision: 0 }).format();
-                    }
-                },
-                {
-                    "targets": 5,
-                    "data": null,
-                    "render": function ( data, type, full, meta ) {
-                        return '<button class="btn btn-danger" onclick="deleteTireData('+data.tire_dataId+', \''+data.tire_brandName+' '+data.tire_modelName+' '+data.tire_size+'\')"><i class="fa fa-trash" aria-hidden="true"></i></button>';
-                    }
-                },
-                {"className": "dt-head-center", "targets": [1]},
-                {"className": "dt-center", "targets": [0,3,4,5]}
-            ]
-    });
-
-    function deleteTireData(tireId, data_name){
-        var option = {
-            url: "/Tiredata/delete?tire_dataId="+tireId,
-            label: "ลบข้อมูลยาง",
-            content: "คุณต้องการลบ <strong>"+data_name+"</strong> นี้ ใช่หรือไม่",
-            gotoUrl: "caraccessory/Tiredata"
+var table = $('#brand-table').DataTable({
+    "language": {
+        "aria": {
+            "sortAscending": ": activate to sort column ascending",
+            "sortDescending": ": activate to sort column descending"
+        },
+        "sProcessing": "กำลังดำเนินการ...",
+        "emptyTable": "ไม่พบข้อมูล",
+        "info": "แสดง _START_ ถึง _END_ ของ _TOTAL_ รายการ",
+        "infoEmpty": "ไม่พบข้อมูล",
+        "infoFiltered": "(กรองข้อมูล _MAX_ ทุกแถว)",
+        "lengthMenu": "_MENU_ รายการ",
+        "zeroRecords": "ไม่พบข้อมูล",
+        "oPaginate": {
+            "sFirst": "หน้าแรก", // This is the link to the first page
+            "sPrevious": "ก่อนหน้า", // This is the link to the previous page
+            "sNext": "ถัดไป", // This is the link to the next page
+            "sLast": "หน้าสุดท้าย" // This is the link to the last page
         }
-        fnDelete(option);
-    }
+    },
+    "responsive": true,
+    "bLengthChange": false,
+    "searching": false,
+    "processing": true,
+    "serverSide": true,
+    "orderable": false,
+    "pageLength": 10,
+    "ajax": {
+        "url": base_url + "apicaraccessories/Tiredata/search",
+        "dataType": "json",
+        "type": "POST",
+        "data": function(data) {
+            data.tire_brandId = $("#tire_brandId").val();
+            data.tire_modelId = $("#tire_modelId").val();
+            data.rimId = $("#rimId").val();
+            data.tire_sizeId = $("#tire_sizeId").val();
+            data.price = $("#price").val();
+            data.can_change = $("#can_change").val();
+            data.sort = $("#sort").val();
+        }
+    },
+    "order": [
+        [1, "asc"]
+    ],
+    "columns": [
+        null,
+        {
+            "data": "tire_brandName"
+        },
+        {
+            "data": "tire_modelName"
+        },
+        {
+            "data": "tire_size"
+        },
+        null
+    ],
+    "columnDefs": [{
+            "searchable": false,
+            "orderable": false,
+            "targets": [0]
+        },
+        {
+            "targets": 0,
+            "data": null,
+            "render": function(data, type, full, meta) {
+                return meta.row + 1;
+            }
+        },
+        {
+            "targets": 4,
+            "data": null,
+            "render": function(data, type, full, meta) {
+                return currency(data.price, {
+                    precision: 0
+                }).format();
+            }
+        },
+        {
+            "targets": 5,
+            "data": null,
+            "render": function(data, type, full, meta) {
+                return '<a href="' + base_url + "caraccessory/tiredata/updatetiredata/" + data
+                    .tire_dataId +
+                    '" class="btn btn-warning"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> ' +
+                    '<button class="btn btn-danger" onclick="deleteTireData(' + data.tire_dataId +
+                    ', \'' + data.tire_brandName + ' ' + data.tire_modelName + ' ' + data.tire_size +
+                    '\')"><i class="fa fa-trash" aria-hidden="true"></i></button>';
+            }
+        },
+        {
+            "className": "dt-head-center",
+            "targets": [1]
+        },
+        {
+            "className": "dt-center",
+            "targets": [0, 3, 4, 5]
+        }
+    ]
+});
 
+function deleteTireData(tireId, data_name) {
+    var option = {
+        url: "/Tiredata/delete?tire_dataId=" + tireId,
+        label: "ลบข้อมูลยาง",
+        content: "คุณต้องการลบ <strong>" + data_name + "</strong> นี้ ใช่หรือไม่",
+        gotoUrl: "caraccessory/Tiredata"
+    }
+    fnDelete(option);
+}
 </script>
 
 </body>
+
 </html>
--- application/views/caraccessory/tiredata/update/content.php
+++ application/views/caraccessory/tiredata/update/content.php
@@ -3,26 +3,27 @@
     <!-- Bread crumb -->
     <div class="row page-titles">
         <div class="col-md-5 align-self-center">
-            <h3 class="text-primary"> แก้ไขข้อมูลยาง</h3> 
+            <h3 class="text-primary"> แก้ไขข้อมูลยาง</h3>
         </div>
         <div class="col-md-7 align-self-center">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/tiredata"); ?>">ข้อมูลยาง</a></li>
+                <li class="breadcrumb-item"><a href="<?=base_url("caraccessory/tiredata");?>">ข้อมูลยาง</a></li>
                 <li class="breadcrumb-item active">แก้ไขข้อมูล</li>
             </ol>
         </div>
     </div>
     <form id="update-tiredata">
-    <div class="container-fluid">   
-        <div class="row">
-            <div class="col-12">
-                <div class="card card-header-blue">
-                    <input type="hidden" id="tire_dataId" name="tire_dataId" value="<?=$tire_dataId ?>">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-header-blue">
+                        <input type="hidden" id="tire_dataId" name="tire_dataId" value="<?=$tire_dataId?>">
                         <div class="form-body"> <br>
                             <div class="row p-t-20">
                                 <div class="col-md-3">
                                     <div class="form-group">
-                                        <label class="control-label">ยี่ห้อยาง</label><span class="error">*</span> <label id="tire_brandId-error" class="error" for="tire_brandId"></label>
+                                        <label class="control-label">ยี่ห้อยาง</label><span class="error">*</span>
+                                        <label id="tire_brandId-error" class="error" for="tire_brandId"></label>
                                         <div class="input-group input-group-default">
                                             <select class="form-control" id="tire_brandId" name="tire_brandId">
                                                 <option value="">เลือกยี่ห้อยาง</option>
@@ -32,58 +33,51 @@
                                 </div>
                                 <div class="col-md-3">
                                     <div class="form-group">
-                                        <label class="control-label">รุ่นยาง</label><span class="error">*</span> <label id="tire_modelId-error" class="error" for="tire_modelId"></label>
+                                        <label class="control-label">รุ่นยาง</label><span class="error">*</span> <label
+                                            id="tire_modelId-error" class="error" for="tire_modelId"></label>
                                         <div class="input-group input-group-default">
                                             <select class="form-control" id="tire_modelId" name="tire_modelId">
                                                 <option value="">เลือกรุ่นยาง</option>
                                             </select>
                                         </div>
                                     </div>
-                                </div> 
+                                </div>
                                 <div class="col-md-3">
                                     <div class="form-group">
-                                        <label class="control-label">ขอบยาง</label><span class="error">*</span> <label id="rimId-error" class="error" for="rimId"></label>
+                                        <label class="control-label">ขอบยาง</label><span class="error">*</span> <label
+                                            id="rimId-error" class="error" for="rimId"></label>
                                         <div class="input-group input-group-default">
                                             <select class="form-control" id="rimId" name="rimId">
-                                            <option value="">เลือกขอบยาง</option>
+                                                <option value="">เลือกขอบยาง</option>
                                             </select>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-3">
                                     <div class="form-group">
-                                        <label class="control-label">ขนาดยาง</label><span class="error">*</span> <label id="tire_sizeId-error" class="error" for="tire_sizeId"></label>
+                                        <label class="control-label">ขนาดยาง</label><span class="error">*</span> <label
+                                            id="tire_sizeId-error" class="error" for="tire_sizeId"></label>
                                         <div class="input-group input-group-default">
                                             <select class="form-control" id="tire_sizeId" name="tire_sizeId">
                                                 <option value="">เลือกขนาดยาง</option>
                                             </select>
                                         </div>
                                     </div>
-                                </div>           
-                            </div>       
-                            
-                            <div class="row p-t-20">
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">ราคา</label><span class="error">*</span> <label id="price-error" class="error" for="price"></label>
-                                        <div class="input-group input-group-default">
-                                            <input type="number" class="form-control" id="price" name="price" placeholder="ราคา" min=0>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">สั่งจองหรือเปลี่ยนทันที</label><span class="error">*</span>
-                                        <div class="input-group input-group-default">
-                                            <select class="form-control" id="can_change" name="can_change">
-                                                <option value="1">เปลี่ยนทันที</option>
-                                                <option value="2">สั่งจอง</option>
-                                            </select>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
+
                             <div class="row p-t-20">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label class="control-label">ราคา</label><span class="error">*</span> <label
+                                            id="price-error" class="error" for="price"></label>
+                                        <div class="input-group input-group-default">
+                                            <input type="number" class="form-control" id="price" name="price"
+                                                placeholder="ราคา" min=0>
+                                        </div>
+                                    </div>
+                                </div>
+
                                 <div class="col-md-3">
                                     <div class="form-group">
                                         <label class="control-label">การรับประกัน-ปี</label>
@@ -115,26 +109,29 @@
                                     <div class="form-group">
                                         <label class="control-label">การรับประกัน-ระยะทาง (กิโลเมตร)</label>
                                         <div class="input-group input-group-default">
-                                            <input type="number" class="form-control" id="warranty_distance" name="warranty_distance" placeholder="ระยะทาง (กิโลเมตร)" min=0>
+                                            <input type="number" class="form-control" id="warranty_distance"
+                                                name="warranty_distance" placeholder="ระยะทาง (กิโลเมตร)" min=0>
                                         </div>
                                     </div>
                                 </div>
-                            </div>     
+                            </div>
                         </div>
-                        
+
                         <div class="row p-t-20">
                             <div class="col-md-12 card-grid">
-                                <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> บันทึก</button>
-                                <a href="<?=base_url("caraccessory/tiredata/"); ?>">
-                                <button type="button" class="btn btn-inverse"><i class="fa fa-close"></i> ยกเลิก</button>
+                                <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i>
+                                    บันทึก</button>
+                                <a href="<?=base_url("caraccessory/tiredata/");?>">
+                                    <button type="button" class="btn btn-inverse"><i class="fa fa-close"></i>
+                                        ยกเลิก</button>
                                 </a>
                             </div>
                         </div>
-                    </form>            
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>   
+    </form>
+</div>
+</div>
+</div>
+</div>
+</div>
 </div>
 <!-- End Container fluid  -->
